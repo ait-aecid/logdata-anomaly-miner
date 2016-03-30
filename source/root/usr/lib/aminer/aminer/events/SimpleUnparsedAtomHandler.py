452d4c0ac07d35e580cac548a053bdd7f1ca3bed
Roman Fiedler <roman.fiedler@ait.ac.at>
2016-03-30 15:08:16 +0000

    Minor: variable renamed

--- source/root/usr/lib/aminer/aminer/events/SimpleUnparsedAtomHandler.py
+++ source/root/usr/lib/aminer/aminer/events/SimpleUnparsedAtomHandler.py
@@ -1,8 +1,10 @@
 class SimpleUnparsedAtomHandler:
+  """Handlers of this class will just forward received unparsed
+  atoms to the registered event handlers."""
 
-  def __init__(self, anomalyEventHandlers):
-    self.anomalyEventHandlers=anomalyEventHandlers
+  def __init__(self, eventHandlers):
+    self.eventHandlers=eventHandlers
 
   def receiveUnparsedAtom(self, atomData, unparsedAtomData, match):
-    for listener in self.anomalyEventHandlers:
+    for listener in self.eventHandlers:
       listener.receiveEvent('ParserModel.UnparsedData', 'Unparsed data', [atomData], match)
