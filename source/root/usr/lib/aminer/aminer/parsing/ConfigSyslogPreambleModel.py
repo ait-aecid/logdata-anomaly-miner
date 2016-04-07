062b78d4a6bbd75fd48b88614bee46a714a17e46
Roman Fiedler <roman.fiedler@ait.ac.at>
2016-04-07 16:54:12 +0000

    Add support for custom time models.

--- source/root/usr/lib/aminer/aminer/parsing/ConfigSyslogPreambleModel.py
+++ source/root/usr/lib/aminer/aminer/parsing/ConfigSyslogPreambleModel.py
@@ -3,10 +3,18 @@ import FixedDataModelElement
 import SequenceModelElement
 import VariableByteDataModelElement
 
-"""This class defines the model for parsing a standard syslog
-preamble including timestamp and hostname."""
-def getModel():
-  timeModel=DateTimeModelElement.DateTimeModelElement('time', '%b %d %H:%M:%S', 15, False)
+def getModel(timeModel=None):
+  """This function defines the model for parsing a standard syslog
+  preamble including timestamp and hostname.
+  @param timeModel when not none, the given model element is used
+  for parsing timestamps. Otherwise a standard DateTimeModelElement
+  with format '%b %d %H:%M:%S' is created. CAVEAT: the standard
+  model may not work when log data timestamp locale does not match
+  host or shell environment locale. See MultiLocaleDateTimeModelElement
+  instead.
+  """
+  if timeModel==None:
+    timeModel=DateTimeModelElement.DateTimeModelElement('time', '%b %d %H:%M:%S', 15, False)
   hostNameModel=VariableByteDataModelElement.VariableByteDataModelElement('host', '-.01234567890abcdefghijklmnopqrstuvwxyz')
   model=SequenceModelElement.SequenceModelElement('syslog', [timeModel, FixedDataModelElement.FixedDataModelElement('sp0', ' '), hostNameModel, FixedDataModelElement.FixedDataModelElement('sp1', ' ')])
   return(model)
