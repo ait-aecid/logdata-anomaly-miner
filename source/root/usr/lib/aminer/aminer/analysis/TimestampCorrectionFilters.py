a8986e7f83429f2a1acbce3d0c7a732a94a4ac61
Roman Fiedler <roman.fiedler@ait.ac.at>
2016-04-07 16:57:13 +0000

    Initial commit of component to correct timestamps for inconsistent datasets.

---
+++ source/root/usr/lib/aminer/aminer/analysis/TimestampCorrectionFilters.py
@@ -0,0 +1,31 @@
+# This file collects various classes useful to filter and correct
+# the timestamp associated with a received parsed atom.
+
+
+class SimpleMonotonicTimestampAdjust:
+  """Handlers of this class compare the timestamp of a newly received
+  atom with the largest timestamp seen so far. When below, the
+  timestamp of this atom is adjusted to the largest value seen,
+  otherwise the largest value seen is updated."""
+  def __init__(self, subhandlerList, stopWhenHandledFlag=False):
+    self.subhandlerList=subhandlerList
+    self.stopWhenHandledFlag=stopWhenHandledFlag
+    self.latestTimestampSeen=0
+
+  def receiveParsedAtom(self, atomData, match):
+    """Pass the atom to the subhandlers.
+    @return false when no subhandler was able to handle the atom."""
+    timestamp=parserMatch.getDefaultTimestamp()
+    if timestamp<self.latestTimestampSeen:
+      parserMatch.setDefaultTimestamp(self.latestTimestampSeen)
+    else:
+      self.latestTimestampSeen=timestamp
+
+    result=False
+    for handler in self.subhandlerList:
+      handlerResult=handler.receiveParsedAtom(atomData, match)
+      if handlerResult==True:
+        result=True
+        if self.stopWhenHandledFlag:
+          break
+    return(result)
