b449e2bef38047a97765e8675fac89e850e05d65
Roman Fiedler <roman.fiedler@ait.ac.at>
2016-07-18 16:44:18 +0000

    Add the namespace references to parser classes defined in the parsing submodule to ease access.

--- source/root/usr/lib/logdata-anomaly-miner/aminer/parsing/__init__.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/parsing/__init__.py
@@ -15,3 +15,28 @@ class ParsedAtomHandlerInterface:
     may decide if it makes sense passing the parsed atom also
     to other handlers."""
     raise Exception('Not implemented')
+
+
+# Add also the namespace references to classes defined in this
+# directory.
+
+from AnyByteDataModelElement import AnyByteDataModelElement
+from DateTimeModelElement import DateTimeModelElement
+from DebugModelElement import DebugModelElement
+from DecimalIntegerValueModelElement import DecimalIntegerValueModelElement
+from DelimitedDataModelElement import DelimitedDataModelElement
+from FirstMatchModelElement import FirstMatchModelElement
+from FixedDataModelElement import FixedDataModelElement
+from FixedWordlistDataModelElement import FixedWordlistDataModelElement
+from HexStringModelElement import HexStringModelElement
+from IpAddressDataModelElement import IpAddressDataModelElement
+from MatchContext import MatchContext
+from MatchElement import MatchElement
+from MultiLocaleDateTimeModelElement import MultiLocaleDateTimeModelElement
+from OptionalMatchModelElement import OptionalMatchModelElement
+from ParserMatch import ParserMatch
+from RepeatedElementDataModelElement import RepeatedElementDataModelElement
+from SequenceModelElement import SequenceModelElement
+from SimpleParsingModelRawAtomHandler import SimpleParsingModelRawAtomHandler
+from VariableByteDataModelElement import VariableByteDataModelElement
+from WhiteSpaceLimitedDataModelElement import WhiteSpaceLimitedDataModelElement
