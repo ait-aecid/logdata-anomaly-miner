aed896d8563546d7283232a86f680e25875261ef
Roman Fiedler <roman.fiedler@ait.ac.at>
2016-07-18 15:00:32 +0000

    Moved EventHandlerInterface interface from separate file to __init__ to collect all submodule interfaces thers (same as in other submodules).

---
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/events/__init__.py
@@ -0,0 +1,24 @@
+# This file contains interface definition useful implemented by
+# classes in this directory and for use from code outside this
+# directory. All classes are defined in separate files, only the
+# namespace references are added here to simplify the code.
+
+class EventHandlerInterface:
+  """This is the common interface of all components that can be
+  notified on significant log data mining events. To avoid interference
+  with the analysis process, the listener may only perform fast
+  actions within the call. Longer running tasks have to be performed
+  asynchronously."""
+
+  def receiveEvent(self, eventType, eventMessage, sortedLogLines, eventData):
+    """Receive information about a detected event.
+    @param eventType is a string with the event type class this
+    event belongs to. This information can be used to interpret
+    type-specific eventData objects. Together with the eventMessage
+    and sortedLogLines, this can be used to create generic log messages.
+    @param sortedLogLines sorted list of log lines that were considered
+    when generating the event, as far as available to the time
+    of the event. The list has to contain at least one line.
+    @param eventData type-specific event data object, should not
+    be used unless listener really knows about the eventType."""
+    raise Exception('Not implemented')
