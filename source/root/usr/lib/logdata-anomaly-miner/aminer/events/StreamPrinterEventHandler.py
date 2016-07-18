33ae30e8f1ac018b0a90b55c1ccbe21cfb9568c6
Roman Fiedler <roman.fiedler@ait.ac.at>
2016-07-18 16:52:35 +0000

    Updated to shorter class access scheme. Removed dead code.

--- source/root/usr/lib/logdata-anomaly-miner/aminer/events/StreamPrinterEventHandler.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/events/StreamPrinterEventHandler.py
@@ -21,14 +21,10 @@ is stdout"""
     for line in sortedLogLines:
       message+='  '+line+'\n'
     if eventData!=None:
-      if isinstance(eventData, MatchElement.MatchElement):
+      if isinstance(eventData, MatchElement):
         message+='  '+eventData.annotateMatch('')+'\n'
       else:
         message+='  '+str(eventData)+'\n'
     print >>self.stream, '%s' % message
     self.stream.flush()
-    return
-
-
-  def checkTriggers(self):
     return
