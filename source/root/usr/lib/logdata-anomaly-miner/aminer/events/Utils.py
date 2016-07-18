ec4e772713c8f3536159024c3074cb133b85ba58
Roman Fiedler <roman.fiedler@ait.ac.at>
2016-07-18 16:39:32 +0000

    Added VolatileLogarithmicBackoffEventHistory to keep data of limited number of events in memory for automated processing.

---
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/events/Utils.py
@@ -0,0 +1,17 @@
+from aminer.events import EventHandlerInterface
+from aminer.util import LogarithmicBackoffHistory
+
+class VolatileLogarithmicBackoffEventHistory(EventHandlerInterface, LogarithmicBackoffHistory):
+  """This class is a volatile filter to keep a history of received
+  events, e.g. for analysis by other components or for external
+  access via remote control interface."""
+
+  def __init__(self, maxItems):
+    """Initialize the history component."""
+    LogarithmicBackoffHistory.__init__(self, maxItems)
+
+  def receiveEvent(self, eventType, eventMessage, sortedLogLines, eventData):
+    """Receive information about a detected event and store all
+    related data as tuple to the history log."""
+    self.addObject((eventType, eventMessage, sortedLogLines, eventData))
+    return(True)
