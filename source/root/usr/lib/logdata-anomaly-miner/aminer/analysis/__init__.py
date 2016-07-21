08ae33489432ea64a56906e1a6aad4c87863f9fe
Roman Fiedler <roman.fiedler@ait.ac.at>
2016-07-21 18:43:54 +0000

    Add the namespace references to event classes defined in the analysis submodule to ease access.

---
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/analysis/__init__.py
@@ -0,0 +1,22 @@
+# This file contains interface definition useful implemented by
+# classes in this directory and for use from code outside this
+# directory. All classes are defined in separate files, only the
+# namespace references are added here to simplify the code.
+
+# No generic interfaces here yet.
+
+# Add also the namespace references to classes defined in this
+# directory.
+
+from MatchValueAverageChangeDetector import MatchValueAverageChangeDetector
+from MatchValueStreamWriter import MatchValueStreamWriter
+from MissingMatchPathValueDetector import MissingMatchPathValueDetector
+from NewMatchPathDetector import NewMatchPathDetector
+from NewMatchPathValueDetector import NewMatchPathValueDetector
+# ParsedAtomFilters.py
+# Rules.py
+from TimeCorrelationDetector import TimeCorrelationDetector
+from TimeCorrelationViolationDetector import TimeCorrelationViolationDetector
+# TimestampCorrectionFilters.py
+# Utils.py
+from WhitelistViolationDetector import WhitelistViolationDetector
