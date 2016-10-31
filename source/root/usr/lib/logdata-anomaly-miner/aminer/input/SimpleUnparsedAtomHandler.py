56b4b62ecde6de1cc2e7df62a7f0a4ffb3c82688
Roman Fiedler <roman.fiedler@ait.ac.at>
2016-10-31 09:55:30 +0000

    Adapted to new AtomHandlerInterface: filter out unparsed atoms and create events only for those.

--- source/root/usr/lib/logdata-anomaly-miner/aminer/input/SimpleUnparsedAtomHandler.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/input/SimpleUnparsedAtomHandler.py
@@ -1,15 +1,16 @@
-from aminer.input import UnparsedAtomHandler
+from aminer.input import AtomHandlerInterface
 
-class SimpleUnparsedAtomHandler(UnparsedAtomHandler):
+class SimpleUnparsedAtomHandler(AtomHandlerInterface):
   """Handlers of this class will just forward received unparsed
   atoms to the registered event handlers."""
 
   def __init__(self, eventHandlers):
     self.eventHandlers=eventHandlers
 
-  def receiveUnparsedAtom(self, message, atomData, unparsedAtomData,
-      parserMatch):
+  def receiveAtom(self, logAtom):
+    """Receive an unparsed atom to create events for each."""
+    if logAtom.isParsed(): return(False)
     for listener in self.eventHandlers:
-      listener.receiveEvent('Input.UnparsedAtomHandler', message,
-          [atomData], [unparsedAtomData, parserMatch], self)
+      listener.receiveEvent('Input.UnparsedAtomHandler',
+          'Unparsed atom received', [logAtom.rawData], logAtom, self)
     return(True)
