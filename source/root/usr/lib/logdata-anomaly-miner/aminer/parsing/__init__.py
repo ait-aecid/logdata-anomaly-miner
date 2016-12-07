e2b99e414bf57d578601f0b8b451da71f70e3a45
Roman Fiedler <roman.fiedler@ait.ac.at>
2016-12-07 08:04:47 +0000

    Clean definition of interface for all ModelElement instances.

--- source/root/usr/lib/logdata-anomaly-miner/aminer/parsing/__init__.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/parsing/__init__.py
@@ -1,6 +1,32 @@
 # Add also the namespace references to classes defined in this
 # directory.
 
+class ModelElementInterface:
+  """This is the superinterface of all model elements."""
+
+  def getId(self):
+    """Get the element ID."""
+    raise Exception('Interface method called')
+
+  def getChildElements(self):
+    """Get all possible child model elements of this element.
+    If this element implements a branching model element, then
+    not all child element IDs will be found in mathces produced
+    by getMatchElement.
+    @return a list with all children"""
+    raise Exception('Interface method called')
+
+  def getMatchElement(self, path, matchContext):
+    """Try to find a match on given data for this model element
+    and all its children. When a match is found, the matchContext
+    is updated accordingly.
+    @param path the model path to the parent model element invoking
+    this method.
+    @param matchContext an instance of MatchContext class holding
+    the data context to match against.
+    @return the matchElement or None if model did not match."""
+
+
 from AnyByteDataModelElement import AnyByteDataModelElement
 from DateTimeModelElement import DateTimeModelElement
 from DebugModelElement import DebugModelElement
