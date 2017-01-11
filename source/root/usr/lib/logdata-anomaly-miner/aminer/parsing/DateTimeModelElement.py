4330a6da62ccd9cd1d3a283a61e5a189b308ce9f
Roman Fiedler <roman.fiedler@ait.ac.at>
2017-01-11 14:55:36 +0000

    Bugfix: handle parsing exceptions when parsing malformed data. Improved code styling using pylint.

--- source/root/usr/lib/logdata-anomaly-miner/aminer/parsing/DateTimeModelElement.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/parsing/DateTimeModelElement.py
@@ -1,10 +1,14 @@
+"""This module contains a datetime parser and helper classes for
+parsing."""
+
 import datetime
 import sys
 import time
 
-import MatchElement
+from aminer.parsing import ModelElementInterface
+from aminer.parsing.MatchElement import MatchElement
 
-class DateTimeModelElement:
+class DateTimeModelElement(ModelElementInterface):
   """This class defines a model element to parse date or datetime
   values. The element is similar to the strptime function but
   does not use it due to the numerous problems associated with
@@ -34,53 +38,55 @@ class DateTimeModelElement:
     @param timeZone the timezone for parsing the values or UTC
     when None.
     @param textLocale the locale to use for parsing the day, month
-    names."""
-    self.id=id
-    self.timeZone=timeZone
+    names or None to use the default locale. Locale changing is
+    not yet implemented, use locale.setlocale() in global configuration."""
+    self.id = id
+    self.timeZone = timeZone
 # Make sure that dateFormat is valid and extract the relevant
 # parts from it.
-    self.formatHasYearFlag=False
-    dateFormatParts=[]
-    dateFormatTypeSet=set()
-    scanPos=0
-    while scanPos<len(dateFormat):
-      nextParamPos=dateFormat.find('%', scanPos)
-      if nextParamPos<0: nextParamPos=len(dateFormat)
-      newElement=None
-      if nextParamPos!=scanPos:
-        newElement=dateFormat[scanPos:nextParamPos]
+    self.formatHasYearFlag = False
+    dateFormatParts = []
+    dateFormatTypeSet = set()
+    scanPos = 0
+    while scanPos < len(dateFormat):
+      nextParamPos = dateFormat.find('%', scanPos)
+      if nextParamPos < 0:
+        nextParamPos = len(dateFormat)
+      newElement = None
+      if nextParamPos != scanPos:
+        newElement = dateFormat[scanPos:nextParamPos]
       else:
-        paramTypeCode=dateFormat[nextParamPos+1]
-        nextParamPos=scanPos+2
-        if paramTypeCode=='%':
-          newElement='%'
-        elif paramTypeCode=='b':
+        paramTypeCode = dateFormat[nextParamPos+1]
+        nextParamPos = scanPos+2
+        if paramTypeCode == '%':
+          newElement = '%'
+        elif paramTypeCode == 'b':
           import calendar
-          nameDict={}
+          nameDict = {}
           for monthPos in range(1, 13):
-            nameDict[calendar.month_name[monthPos][:3]]=monthPos
-          newElement=(1, 3, nameDict)
-        elif paramTypeCode=='d':
-          newElement=(2, 2, int)
-        elif paramTypeCode=='f':
-          newElement=(6, -1, DateTimeModelElement.parseFraction)
-        elif paramTypeCode=='H':
-          newElement=(3, 2, int)
-        elif paramTypeCode=='M':
-          newElement=(4, 2, int)
-        elif paramTypeCode=='m':
-          newElement=(1, 2, int)
-        elif paramTypeCode=='S':
-          newElement=(5, 2, int)
-        elif paramTypeCode=='s':
-          newElement=(7, -1, int)
-        elif paramTypeCode=='Y':
-          newElement=(0, 4, int)
+            nameDict[calendar.month_name[monthPos][:3]] = monthPos
+          newElement = (1, 3, nameDict)
+        elif paramTypeCode == 'd':
+          newElement = (2, 2, int)
+        elif paramTypeCode == 'f':
+          newElement = (6, -1, DateTimeModelElement.parseFraction)
+        elif paramTypeCode == 'H':
+          newElement = (3, 2, int)
+        elif paramTypeCode == 'M':
+          newElement = (4, 2, int)
+        elif paramTypeCode == 'm':
+          newElement = (1, 2, int)
+        elif paramTypeCode == 'S':
+          newElement = (5, 2, int)
+        elif paramTypeCode == 's':
+          newElement = (7, -1, int)
+        elif paramTypeCode == 'Y':
+          newElement = (0, 4, int)
         else:
           raise Exception('Unknown dateformat specifier %s' % repr(paramTypeCode))
       if isinstance(newElement, str):
-        if (len(dateFormatParts)>0) and (isinstance(dateFormatParts[-1], str)):
-          dateFormatParts[-1]+=newElement
+        if (len(dateFormatParts) > 0) and (isinstance(dateFormatParts[-1], str)):
+          dateFormatParts[-1] += newElement
         else:
           dateFormatParts.append(newElement)
       else:
@@ -88,91 +94,104 @@ class DateTimeModelElement:
           raise Exception('Multiple format specifiers for type %d' % newElement[0])
         dateFormatTypeSet.add(newElement[0])
         dateFormatParts.append(newElement)
-      scanPos=nextParamPos
-    if (7 in dateFormatTypeSet) and (not(dateFormatTypeSet.isdisjoint(set(range(0, 6))))):
+      scanPos = nextParamPos
+    if (7 in dateFormatTypeSet) and (not dateFormatTypeSet.isdisjoint(set(range(0, 6)))):
       raise Exception('Cannot use %%s (seconds since epoch) with other non-second format types')
-    self.dateFormatParts=dateFormatParts
+    self.dateFormatParts = dateFormatParts
 
-    self.startYear=startYear
-    if (not(self.formatHasYearFlag)) and (startYear==None):
-      self.startYear=time.gmtime(None).tm_year
-    self.lastParsedSeconds=0
-    self.epochStartTime=datetime.datetime.fromtimestamp(0, self.timeZone)
+    self.startYear = startYear
+    if (not self.formatHasYearFlag) and (startYear is None):
+      self.startYear = time.gmtime(None).tm_year
+    self.lastParsedSeconds = 0
+    self.epochStartTime = datetime.datetime.fromtimestamp(0, self.timeZone)
 
   def getChildElements(self):
-    return(None)
+    """Get all possible child model elements of this element.
+    @return None as no children are allowed."""
+    return None
 
   def getMatchElement(self, path, matchContext):
-    """@return None when there is no match, MatchElement otherwise.
+    """Try to find a match on given data for this model element
+    and all its children. When a match is found, the matchContext
+    is updated accordingly.
+    @return None when there is no match, MatchElement otherwise.
     The matchObject returned is a tuple containing the datetime
     object and the seconds since 1970"""
-    parsePos=0
+    parsePos = 0
 # Year, month, day, hour, minute, second, fraction, gmt-seconds:
-    result=[None, None, None, None, None, None, None, None]
+    result = [None, None, None, None, None, None, None, None]
     for partPos in range(0, len(self.dateFormatParts)):
-      dateFormatPart=self.dateFormatParts[partPos]
+      dateFormatPart = self.dateFormatParts[partPos]
       if isinstance(dateFormatPart, str):
-        if not(matchContext.matchData[parsePos:].startswith(dateFormatPart)):
-          return(None)
-        parsePos+=len(dateFormatPart)
+        if not matchContext.matchData[parsePos:].startswith(dateFormatPart):
+          return None
+        parsePos += len(dateFormatPart)
         continue
-      nextLength=dateFormatPart[1]
-      nextData=None
-      if nextLength<0:
+      nextLength = dateFormatPart[1]
+      nextData = None
+      if nextLength < 0:
 # No length given: this is only valid for integer fields or fields
 # followed by a separator string.
-        if (partPos+1)<len(self.dateFormatParts):
-          nextPart=self.dateFormatParts[partPos+1]
+        if (partPos+1) < len(self.dateFormatParts):
+          nextPart = self.dateFormatParts[partPos+1]
           if isinstance(nextPart, str):
-            endPos=matchContext.matchData.find(nextPart, parsePos)
-            if endPos<0:
-              return(None)
-            nextLength=endPos-parsePos
-        if nextLength<0:
+            endPos = matchContext.matchData.find(nextPart, parsePos)
+            if endPos < 0:
+              return None
+            nextLength = endPos-parsePos
+        if nextLength < 0:
 # No separator, so get the number of decimal digits.
-          nextLength=0
+          nextLength = 0
           for digitChar in matchContext.matchData[parsePos:]:
-            digitOrd=ord(digitChar)
-            if (digitOrd<0x30) or (digitOrd>0x39): break
-            nextLength+=1
-          if nextLength==0:
-            return(None)
-        nextData=matchContext.matchData[parsePos:parsePos+nextLength]
+            digitOrd = ord(digitChar)
+            if (digitOrd < 0x30) or (digitOrd > 0x39):
+              break
+            nextLength += 1
+          if nextLength == 0:
+            return None
+        nextData = matchContext.matchData[parsePos:parsePos+nextLength]
       else:
-        nextData=matchContext.matchData[parsePos:parsePos+nextLength]
-        if len(nextData)!=nextLength:
-          return(None)
-      parsePos+=nextLength
-      transformFunction=dateFormatPart[2]
+        nextData = matchContext.matchData[parsePos:parsePos+nextLength]
+        if len(nextData) != nextLength:
+          return None
+      parsePos += nextLength
+      transformFunction = dateFormatPart[2]
       if isinstance(transformFunction, dict):
-        value=None
+        value = None
         try:
-          value=transformFunction.get(nextData, None)
-        except ValueError as transformError:
+          value = transformFunction.get(nextData, None)
+        except ValueError:
           pass
-        if value==None:
-          return(None)
-        result[dateFormatPart[0]]=value
+        if value is None:
+          return None
+        result[dateFormatPart[0]] = value
       else:
-        result[dateFormatPart[0]]=transformFunction(nextData)
-    dateStr=matchContext.matchData[:parsePos]
+        try:
+          result[dateFormatPart[0]] = transformFunction(nextData)
+        except:
+# Parsing failed, most likely due to wrong format.
+          return None
+
+    dateStr = matchContext.matchData[:parsePos]
 
 # Now combine the values and build the final value.
-    parsedDateTime=None
-    totalSeconds=result[7]
-    if totalSeconds!=None:
-      if result[6]!=None:
-        totalSeconds+=result[6]
+    parsedDateTime = None
+    totalSeconds = result[7]
+    if totalSeconds != None:
+      if result[6] != None:
+        totalSeconds += result[6]
 # For epoch second formats, the datetime value usually is not
 # important. So stay with parsedDateTime to none.
     else:
-      if not(self.formatHasYearFlag):
-        result[0]=self.startYear
-      microseconds=0
-      if result[6]!=None:
-        microseconds=int(result[6]*1000000)
+      if not self.formatHasYearFlag:
+        result[0] = self.startYear
+      microseconds = 0
+      if result[6] != None:
+        microseconds = int(result[6]*1000000)
       try:
-        parsedDateTime=datetime.datetime(result[0], result[1], result[2], result[3], result[4], result[5], microseconds, self.timeZone)
+        parsedDateTime = datetime.datetime(
+            result[0], result[1], result[2], result[3], result[4],
+            result[5], microseconds, self.timeZone)
       except:
 # The values did not form a valid datetime object, e.g. when the
 # day of month is out of range. The rare case where dates without
@@ -180,31 +199,36 @@ class DateTimeModelElement:
 # non-leap year but the current timestamp is it, is ignored. Values
 # that sparse and without a year number are very likely to result
 # in invalid data anyway.
-        raise
-        return(None)
+        return None
 
 # Avoid timedelta.total_seconds(), not supported in Python 2.6.
-      delta=parsedDateTime-self.epochStartTime
-      totalSeconds=(delta.days*86400+delta.seconds)
+      delta = parsedDateTime-self.epochStartTime
+      totalSeconds = (delta.days*86400+delta.seconds)
 
 # See if this is change from one year to next.
-      if (not(self.formatHasYearFlag)) and (totalSeconds<self.lastParsedSeconds-3600*24*7):
-        print >>sys.stderr, 'WARNING: DateTimeModelElement unqualified timestamp year wraparound detected from %s to %s' % (datetime.datetime.fromtimestamp(self.lastParsedSeconds, self.timeZone).isoformat(), parsedDateTime.isoformat())
-        self.startYear+=1
-        parsedDateTime=parsedDateTime.replace(self.startYear)
+      if ((not self.formatHasYearFlag) and
+          (totalSeconds < self.lastParsedSeconds-3600*24*7)):
+        print >>sys.stderr, 'WARNING: DateTimeModelElement unqualified ' \
+            'timestamp year wraparound detected from %s to %s' % (
+                datetime.datetime.fromtimestamp(
+                    self.lastParsedSeconds, self.timeZone).isoformat(),
+                parsedDateTime.isoformat())
+        self.startYear += 1
+        parsedDateTime = parsedDateTime.replace(self.startYear)
 
-      self.lastParsedSeconds=totalSeconds
+      self.lastParsedSeconds = totalSeconds
 # We discarded the parsedDateTime microseconds beforehand, use
 # the full float value here instead of the rounded integer.
-      if result[6]!=None:
-        totalSeconds+=result[6]
+      if result[6] != None:
+        totalSeconds += result[6]
 
     matchContext.update(dateStr)
-    return(MatchElement.MatchElement("%s/%s" % (path, self.id),
-        dateStr, (parsedDateTime, totalSeconds,), None))
+    return MatchElement(
+        "%s/%s" % (path, self.id), dateStr, (parsedDateTime, totalSeconds,),
+        None)
 
   @staticmethod
-  def parseFraction(str):
+  def parseFraction(valueStr):
     """This method is just required to pass it as function pointer
     to the parsing logic."""
-    return(float('0.'+str))
+    return float('0.'+valueStr)
