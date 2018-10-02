1d7b2606577043e62f02509b8aa4bba64bf04477
Markus Wurzenberger <markus.wurzenberger@ait.ac.at>
2018-10-02 16:48:08 +0200

    Fix typo in import of DecimalFloatValueModelElement. (line 3) Fix typo in model definition. (line 67) Linter file with pylint3.

--- source/root/etc/aminer/conf-available/generic/NtpParsingModel.py
+++ source/root/etc/aminer/conf-available/generic/NtpParsingModel.py
@@ -1,6 +1,6 @@
 """This module defines the parsing model for ntpd logs."""
 
-from aminer.parsing import DecimalFloatValueModelElemen
+from aminer.parsing import DecimalFloatValueModelElement
 from aminer.parsing import DecimalIntegerValueModelElement
 from aminer.parsing import DelimitedDataModelElement
 from aminer.parsing import FirstMatchModelElement
@@ -12,65 +12,65 @@ from aminer.parsing import VariableByteDataModelElement
 def getModel():
   """Get the model."""
   interfaceNameModel = VariableByteDataModelElement(
-      'interface', '0123456789abcdefghijklmnopqrstuvwxyz.')
+      'interface', b'0123456789abcdefghijklmnopqrstuvwxyz.')
 
   typeChildren = []
   typeChildren.append(SequenceModelElement('exit', [
-      FixedDataModelElement('s0', 'ntpd exiting on signal '),
+      FixedDataModelElement('s0', b'ntpd exiting on signal '),
       DecimalIntegerValueModelElement('signal')
   ]))
 
   typeChildren.append(SequenceModelElement('listen-drop', [
-      FixedDataModelElement('s0', 'Listen and drop on '),
+      FixedDataModelElement('s0', b'Listen and drop on '),
       DecimalIntegerValueModelElement('fd'),
-      FixedDataModelElement('s1', ' '),
+      FixedDataModelElement('s1', b' '),
       interfaceNameModel,
-      FixedDataModelElement('s2', ' '),
+      FixedDataModelElement('s2', b' '),
       FirstMatchModelElement('address', [
           IpAddressDataModelElement('ipv4'),
-          DelimitedDataModelElement('ipv6', ' '),
+          DelimitedDataModelElement('ipv6', b' '),
       ]),
-      FixedDataModelElement('s3', ' UDP 123')
+      FixedDataModelElement('s3', b' UDP 123')
   ]))
 
   typeChildren.append(SequenceModelElement('listen-normal', [
-      FixedDataModelElement('s0', 'Listen normally on '),
+      FixedDataModelElement('s0', b'Listen normally on '),
       DecimalIntegerValueModelElement('fd'),
-      FixedDataModelElement('s1', ' '),
+      FixedDataModelElement('s1', b' '),
       interfaceNameModel,
-      FixedDataModelElement('s2', ' '),
+      FixedDataModelElement('s2', b' '),
       IpAddressDataModelElement('ip'),
       FirstMatchModelElement('msg', [
-          FixedDataModelElement('port-new', ':123'),
-          FixedDataModelElement('port-old', ' UDP 123')
+          FixedDataModelElement('port-new', b':123'),
+          FixedDataModelElement('port-old', b' UDP 123')
       ])
   ]))
 
   typeChildren.append(SequenceModelElement('listen-routing', [
-      FixedDataModelElement('s0', 'Listening on routing socket on fd #'),
+      FixedDataModelElement('s0', b'Listening on routing socket on fd #'),
       DecimalIntegerValueModelElement('fd'),
-      FixedDataModelElement('s1', ' for interface updates')
+      FixedDataModelElement('s1', b' for interface updates')
   ]))
 
   typeChildren.append(FixedDataModelElement(
-      'new-interfaces', 'new interface(s) found: waking up resolver'))
+      'new-interfaces', b'new interface(s) found: waking up resolver'))
 
   typeChildren.append(FixedDataModelElement(
       'ntp-io',
-      'ntp_io: estimated max descriptors: 1024, initial socket boundary: 16'))
+      b'ntp_io: estimated max descriptors: 1024, initial socket boundary: 16'))
 
   typeChildren.append(FixedDataModelElement(
-      'peers-refreshed', 'peers refreshed'))
+      'peers-refreshed', b'peers refreshed'))
 
   typeChildren.append(SequenceModelElement('precision', [
-      FixedDataModelElement('s0', 'proto: precision = '),
-      DecimalFloatValueModelElemen('precision'),
-      FixedDataModelElement('s1', ' usec')
+      FixedDataModelElement('s0', b'proto: precision = '),
+      DecimalFloatValueModelElement('precision'),
+      FixedDataModelElement('s1', b' usec')
   ]))
 
   model = SequenceModelElement('ntpd', [
-      FixedDataModelElement('sname', 'ntpd['),
+      FixedDataModelElement('sname', b'ntpd['),
       DecimalIntegerValueModelElement('pid'),
-      FixedDataModelElement('s0', ']: '),
+      FixedDataModelElement('s0', b']: '),
       FirstMatchModelElement('msg', typeChildren)])
   return model
