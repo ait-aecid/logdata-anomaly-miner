3bb33c9568cb2be307f6a2d43ed7be5c25590dc2
Markus Wurzenberger <markus.wurzenberger@ait.ac.at>
2018-10-02 16:51:43 +0200

    Fix AnalysisContext not defined by adding import statement (line 38) Linter file with pylint3.

--- source/root/usr/lib/logdata-anomaly-miner/aminer/analysis/MatchValueStreamWriter.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/analysis/MatchValueStreamWriter.py
@@ -1,9 +1,13 @@
+"""This module dfines a writer that forwards match information
+to a stream."""
+
+from aminer.AnalysisChild import AnalysisContext
 from aminer.input import AtomHandlerInterface
 from aminer.util import TimeTriggeredComponentInterface
 
 class MatchValueStreamWriter(AtomHandlerInterface, TimeTriggeredComponentInterface):
   """This class extracts values from a given match and writes
-  them to a stream. This can be used to forward this values to
+  them to a stream. This can be used to forward these values to
   another program (when stream is a wrapped network socket) or
   to a file for further analysis. A stream is used instead of
   a file descriptor to increase performance. To flush it from
@@ -12,21 +16,25 @@ class MatchValueStreamWriter(AtomHandlerInterface, TimeTriggeredComponentInterfa
 
   def __init__(self, stream, matchValuePathList, separatorString, missingValueString):
     """Initialize the writer."""
-    self.stream=stream
-    self.matchValuePathList=matchValuePathList
-    self.separatorString=separatorString
-    self.missingValueString=missingValueString
+    self.stream = stream
+    self.matchValuePathList = matchValuePathList
+    self.separatorString = separatorString
+    self.missingValueString = missingValueString
 
   def receiveAtom(self, logAtom):
-    matchDict=logAtom.parserMatch.getMatchDictionary()
-    addSepFlag=False
-    result=b''
+    """Forward match value information to the stream."""
+    matchDict = logAtom.parserMatch.getMatchDictionary()
+    addSepFlag = False
+    result = b''
     for path in self.matchValuePathList:
-      if addSepFlag: result+=self.separatorString
-      match=matchDict.get(path, None)
-      if match==None: result+=self.missingValueString
-      else: result+=match.matchString
-      addSepFlag=True
+      if addSepFlag:
+        result += self.separatorString
+      match = matchDict.get(path, None)
+      if match is None:
+        result += self.missingValueString
+      else:
+        result += match.matchString
+      addSepFlag = True
     self.stream.write(result)
     self.stream.write('\n')
 
@@ -35,11 +43,13 @@ class MatchValueStreamWriter(AtomHandlerInterface, TimeTriggeredComponentInterfa
     """Get the trigger class this component should be registered
     for. This trigger is used only for persistency, so real-time
     triggering is needed."""
-    return(AnalysisContext.TIME_TRIGGER_CLASS_REALTIME)
+    return AnalysisContext.TIME_TRIGGER_CLASS_REALTIME
 
-  def doTimer(self, time):
+  def doTimer(self, triggerTime):
+    """Flush the timer."""
     self.stream.flush()
-    return(10)
+    return 10
 
   def doPersist(self):
+    """Flush the timer."""
     self.stream.flush()
