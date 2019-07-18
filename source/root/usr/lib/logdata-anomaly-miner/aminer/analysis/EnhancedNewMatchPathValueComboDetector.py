3fc7723b7f43c2e65db3af4d74688dfbf2f2f2ac
Ernst Leierzopf <e.leierzopf@gmail.com>
2019-07-18 18:44:50 +0200

    changed EnhancedNewMatchPathValueComboDetector.receiveEvent to comply with the EventData requirements.

--- source/root/usr/lib/logdata-anomaly-miner/aminer/analysis/EnhancedNewMatchPathValueComboDetector.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/analysis/EnhancedNewMatchPathValueComboDetector.py
@@ -90,10 +90,8 @@ class EnhancedNewMatchPathValueComboDetector(NewMatchPathValueComboDetector):
           self.nextPersistTime = time.time()+600
       for listener in self.anomalyEventHandlers:
         listener.receiveEvent(
-            'Analysis.%s' % self.__class__.__name__,
-            'New value combination for path(es) %s: %s' % (
-                ', '.join(self.targetPathList), repr(matchValueTuple)),
-            [logAtom.rawData], [logAtom, matchValueTuple], self)
+            'Analysis.%s' % self.__class__.__name__, 'New value combinations detected',
+            [logAtom.parserMatch.matchElement.annotateMatch('')], logAtom, self)
     return True
 
 
