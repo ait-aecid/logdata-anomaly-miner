ef842c13cde0cd5ca33a190ec5b764e776d26c78
eleierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-02-07 19:24:34 +0100

    test

--- source/root/usr/lib/logdata-anomaly-miner/aminer/analysis/Rules.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/analysis/Rules.py
@@ -431,7 +431,7 @@ class ValueDependentModuloTimeMatchRule(MatchRule):
       testValue = timeMatch.matchObject[1] + datetime.datetime.now(self.tzinfo).utcoffset().total_seconds()
 
     if testValue is None:
-      print("return False 3dd")
+      print("return False 3ddd")
       return False
     testValue %= self.secondsModulo
     print("testValue %d"%testValue)
