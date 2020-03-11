fc7f05daf1ef1d977ca033d5dcdbbe1d9e849229
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-03-11 07:40:12 +0100

    fixed issues in generic models.

--- source/root/etc/aminer/conf-available/generic/AMinerParsingModel.py
+++ source/root/etc/aminer/conf-available/generic/AMinerParsingModel.py
@@ -6,25 +6,25 @@ from aminer.parsing import FirstMatchModelElement
 from aminer.parsing import FixedDataModelElement
 from aminer.parsing import SequenceModelElement
 
-def getModel():
+def get_model():
   """This method returns the model."""
-  typeChildren = []
-  typeChildren.append(FixedDataModelElement('warn-no-openat', \
+  type_children = []
+  type_children.append(FixedDataModelElement('warn-no-openat', \
           b'WARNING: SECURITY: No secure open yet due to missing openat in python!'))
-  typeChildren.append(FixedDataModelElement('warn-no-OPATH', \
+  type_children.append(FixedDataModelElement('warn-no-OPATH', \
           b'WARNING: SECURITY: Open should use O_PATH, but not yet available in python'))
-  typeChildren.append(FixedDataModelElement('warn-POSIX-acls', \
+  type_children.append(FixedDataModelElement('warn-POSIX-acls', \
           b'WARNING: SECURITY: No checking for backdoor access via \
           POSIX ACLs, use "getfacl" from "acl" package to check manually.'))
-  typeChildren.append(FixedDataModelElement('warn-no-linkat', \
+  type_children.append(FixedDataModelElement('warn-no-linkat', \
           b'WARNING: SECURITY: unsafe unlink (unavailable unlinkat/linkat \
           should be used, but not available in python)'))
 
-  typeChildren.append(AnyByteDataModelElement('unparsed'))
+  type_children.append(AnyByteDataModelElement('unparsed'))
 
   model = SequenceModelElement('aminer', [
       FixedDataModelElement('sname', b'AMiner['),
       DecimalIntegerValueModelElement('pid'),
       FixedDataModelElement('s0', b']: '),
-      FirstMatchModelElement('msg', typeChildren)])
+      FirstMatchModelElement('msg', type_children)])
   return model
