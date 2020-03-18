46b4d104388e8522f9472d2bf1e385141e63adc1
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-03-18 09:31:06 +0100

    test

--- source/root/usr/lib/logdata-anomaly-miner/aminer/parsing/DelimitedDataModelElement.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/parsing/DelimitedDataModelElement.py
@@ -3,13 +3,16 @@ to a specific delimiter string."""
 
 from aminer.parsing.MatchElement import MatchElement
 from aminer.parsing import ModelElementInterface
+import re
+
 
 class DelimitedDataModelElement(ModelElementInterface):
-  """Find a string delimited by given delimiter string, possibly
-  a match of zero byte length"""
-  def __init__(self, element_id, delimiter):
+  """Find a string delimited by given non-escaped delimiter string,
+  possibly a match of zero byte length"""
+  def __init__(self, element_id, delimiter, escape=None):
     self.element_id = element_id
     self.delimiter = delimiter
+    self.escape = escape
 
   def get_child_elements(self):
     """Get all possible child model elements of this element.
@@ -18,11 +21,19 @@ class DelimitedDataModelElement(ModelElementInterface):
 
   def get_match_element(self, path, match_context):
     """Find the maximum number of bytes before encountering the
-    delimiter.
+    non-escaped delimiter.
     @return a match when at least one byte was found but not the
     delimiter itself."""
-    data = match_context.match_data
-    match_len = data.find(self.delimiter)
+    data = match_context.matchData
+    match_len = -1
+    if self.escape is None:
+      search = re.search(re.escape(self.delimiter), data)
+      if search is not None:
+        match_len = search.start()
+    else:
+      search = re.search(rb'(?<!' + re.escape(self.escape) + rb')' + re.escape(self.delimiter), data)
+      if search is not None:
+        match_len = search.start()
     if match_len < 1:
       return None
     match_data = data[:match_len]
