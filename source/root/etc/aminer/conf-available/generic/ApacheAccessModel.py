2038fefec2e8a8198a51c0662ae9c7a83dc12802
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-03-19 08:33:36 +0100

    fixed issues in ApacheAccessModel.

--- source/root/etc/aminer/conf-available/generic/ApacheAccessModel.py
+++ source/root/etc/aminer/conf-available/generic/ApacheAccessModel.py
@@ -1,6 +1,6 @@
 """This module defines a parser for apache2 access.log."""
 
-from aminer.parsing import DateTimeModelElement
+from aminer.parsing import Datetime_modelElement
 from aminer.parsing import DecimalIntegerValueModelElement
 from aminer.parsing import FixedDataModelElement
 from aminer.parsing import SequenceModelElement
@@ -8,41 +8,40 @@ from aminer.parsing import VariableByteDataModelElement
 from aminer.parsing import AnyByteDataModelElement
 from aminer.parsing import FixedWordlistDataModelElement
 
-def getModel(timeModel=None):
+def get_model(time_model=None):
 
-  timeModel = DateTimeModelElement('time', b'[%d/%b/%Y:%H:%M:%S +0000]')
-  hostNameModel = VariableByteDataModelElement('host', b'-.01234567890abcdefghijklmnopqrstuvwxyz:')
-  identityModel = VariableByteDataModelElement('ident', b'-.01234567890abcdefghijklmnopqrstuvwxyz:')
-  userNameModel = VariableByteDataModelElement('user', b'0123456789abcdefghijklmnopqrstuvwxyz.-')
-  requestMethodModel = FixedWordlistDataModelElement('method', [b'GET', b'POST', b'PUT', b'HEAD', b'DELETE', b'CONNECT', b'OPTIONS', b'TRACE', b'PATCH'])
-  requestModel = VariableByteDataModelElement('request', b'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.-/()[]{}!$%&=<?*+')
-  versionModel = VariableByteDataModelElement('version', b'0123456789.')
-  statuscodeModel = DecimalIntegerValueModelElement('status')
-  sizeModel = DecimalIntegerValueModelElement('size')
-  useragentModel = VariableByteDataModelElement('useragent', b'0123456789abcdefghijklmnopqrstuvwxyz.-/()[]{}!$%&=<?*+')
+  new_time_model = Datetime_modelElement('time', b'[%d/%b/%Y:%H:%M:%S +0000]')
+  host_name_model = VariableByteDataModelElement('host', b'-.01234567890abcdefghijklmnopqrstuvwxyz:')
+  identity_model = VariableByteDataModelElement('ident', b'-.01234567890abcdefghijklmnopqrstuvwxyz:')
+  user_name_model = VariableByteDataModelElement('user', b'0123456789abcdefghijklmnopqrstuvwxyz.-')
+  request_method_model = FixedWordlistDataModelElement('method', [b'GET', b'POST', b'PUT', b'HEAD', b'DELETE', b'CONNECT', b'OPTIONS', b'TRACE', b'PATCH'])
+  request_model = VariableByteDataModelElement('request', b'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.-/()[]{}!$%&=<?*+')
+  version_model = VariableByteDataModelElement('version', b'0123456789.')
+  status_code_model = DecimalIntegerValueModelElement('status')
+  size_model = DecimalIntegerValueModelElement('size')
+  user_agent_model = VariableByteDataModelElement('useragent', b'0123456789abcdefghijklmnopqrstuvwxyz.-/()[]{}!$%&=<?*+')
 
-
+  whitespace_str = b' '
   model = SequenceModelElement('accesslog', [
-      hostNameModel,
-      FixedDataModelElement('sp0', b' '),
-      identityModel,
-      FixedDataModelElement('sp1', b' '),
-      userNameModel,
-      FixedDataModelElement('sp2', b' '),
-      timeModel,
+      host_name_model,
+      FixedDataModelElement('sp0', whitespace_str),
+      identity_model,
+      FixedDataModelElement('sp1', whitespace_str),
+      user_name_model,
+      FixedDataModelElement('sp2', whitespace_str),
+      new_time_model,
       FixedDataModelElement('sp3', b' "'),
-      requestMethodModel,
-      FixedDataModelElement('sp4', b' '),
-      requestModel,
+      request_method_model,
+      FixedDataModelElement('sp4', whitespace_str),
+      request_model,
       FixedDataModelElement('sp5', b' HTTP/'),
-      versionModel,
-#      AnyByteDataModelElement('any')
+      version_model,
       FixedDataModelElement('sp6', b'" '),
-      statuscodeModel,
-      FixedDataModelElement('sp7', b' '),
-      sizeModel,
+      status_code_model,
+      FixedDataModelElement('sp7', whitespace_str),
+      size_model,
       FixedDataModelElement('sp8', b' "-" "'),
-      useragentModel,
+      user_agent_model,
       FixedDataModelElement('sp9', b'"'),
       ])
   return model
