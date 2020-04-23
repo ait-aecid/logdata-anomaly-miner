92c8312401802c5f4636ec83da3eeb7f77facd1f
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-04-23 21:44:42 +0200

    test

--- aecid-testsuite/integration/declarations.sh
+++ aecid-testsuite/integration/declarations.sh
@@ -40,8 +40,8 @@ function isDate() {
 # This function checks if the input value contains the local UName.
 # $1 = String parameter to check
 function isUname() {
-	#echo "1 $1"
-	#echo "cat `cat /etc/hostname`"
+	echo "1 $1"
+	echo "cat `cat /etc/hostname`"
 	if [[ $# -gt 0 && "$1" == *" `cat /etc/hostname`"* ]]; then
 		return 0
 	fi
