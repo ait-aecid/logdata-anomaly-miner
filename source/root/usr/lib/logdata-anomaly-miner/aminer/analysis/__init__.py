c4e1f314ea06f147b922eb34ecf1f648e7153e15
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-05-17 19:22:14 +0200

    resolved merge issues.

--- source/root/usr/lib/logdata-anomaly-miner/aminer/analysis/__init__.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/analysis/__init__.py
@@ -1,19 +1,29 @@
+"""This file contains interface definition useful implemented by
+classes in this directory and for use from code outside this
+directory. All classes are defined in separate files, only the
+namespace references are added here to simplify the code.
+
+No generic interfaces here yet.
+
+Add also the namespace references to classes defined in this
+directory."""
+
+CONFIG_KEY_LOG_LINE_PREFIX = 'LogPrefix'
+
+# AtomFilters.py
 from aminer.analysis.MatchValueAverageChangeDetector import MatchValueAverageChangeDetector
 from aminer.analysis.MatchValueStreamWriter import MatchValueStreamWriter
 from aminer.analysis.MissingMatchPathValueDetector import MissingMatchPathValueDetector
 from aminer.analysis.MissingMatchPathValueDetector import MissingMatchPathListValueDetector
+from aminer.analysis.NewMatchIdValueComboDetector import NewMatchIdValueComboDetector
 from aminer.analysis.NewMatchPathDetector import NewMatchPathDetector
 from aminer.analysis.NewMatchPathValueComboDetector import NewMatchPathValueComboDetector
 from aminer.analysis.NewMatchPathValueDetector import NewMatchPathValueDetector
+# Rules.py
 from aminer.analysis.TimeCorrelationDetector import TimeCorrelationDetector
 from aminer.analysis.TimeCorrelationViolationDetector import TimeCorrelationViolationDetector
 from aminer.analysis.TimestampsUnsortedDetector import TimestampsUnsortedDetector
+# TimestampCorrectionFilters.py
 from aminer.analysis.WhitelistViolationDetector import WhitelistViolationDetector
-"""This file contains interface definition useful implemented by classes in this directory and for use from code outside this
-directory. All classes are defined in separate files, only the namespace references are added here to simplify the code.
-
-No generic interfaces here yet.
-
-Add also the namespace references to classes defined in this directory."""
-
-CONFIG_KEY_LOG_LINE_PREFIX = 'LogPrefix'
+# Others
+from aminer.analysis.ParserCount import ParserCount
--- source/root/usr/lib/logdata-anomaly-miner/aminer/input/SimpleByteStreamLineAtomizerFactory.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/input/SimpleByteStreamLineAtomizerFactory.py
@@ -1,25 +1,33 @@
-"""This module defines a factory for instantiating line atomizers."""
+"""This module defines a factory for instanciating line atomizers."""
 
 from aminer.input import AtomizerFactory
 from aminer.input.ByteStreamLineAtomizer import ByteStreamLineAtomizer
 
 
 class SimpleByteStreamLineAtomizerFactory(AtomizerFactory):
-    """This factory just creates the same atomizer for each new resource. All parsed and unparsed atoms are delivered via two
-    lists of handlers."""
+  """This factory just creates the same atomizer for each new
+  resource. All parsed and unparsed atoms are delivered via two
+  lists of handlers."""
 
-    def __init__(self, parsing_model, atom_handler_list, event_handler_list, default_timestamp_path=None):
-        """Create the factory to forward data and events to the given lists for each newly created atomizer.
-        @param default_timestamp_path if not None, the value of this timestamp field is extracted from parsed atoms and stored
-        as default timestamp for that atom."""
-        self.parsing_model = parsing_model
-        self.atom_handler_list = atom_handler_list
-        self.event_handler_list = event_handler_list
-        self.default_timestamp_path = default_timestamp_path
+  def __init__(self, parsing_model, atom_handler_list, event_handler_list,
+               default_timestamp_paths=None):
+    """Create the factory to forward data and events to the given
+    lists for each newly created atomizer.
+    @param default_timestamp_paths if not empty list, the value of this
+    timestamp field is extracted from parsed atoms and stored
+    as default timestamp for that atom."""
+    self.parsing_model = parsing_model
+    self.atom_handler_list = atom_handler_list
+    self.event_handler_list = event_handler_list
+    if default_timestamp_paths is None:
+      self.default_timestamp_paths = []
+    else:
+      self.default_timestamp_paths = default_timestamp_paths
 
-    def get_atomizer_for_resource(self, resource_name):
-        """Get an atomizer for a given resource.
-        @param resource_name the resource name for atomizer selection is ignored in this type of factory.
-        @return a StreamAtomizer object"""
-        return ByteStreamLineAtomizer(self.parsing_model, self.atom_handler_list, self.event_handler_list, 1 << 16,
-                                      self.default_timestamp_path)
+  def get_atomizer_for_resource(self, resource_name):
+    """Get an atomizer for a given resource.
+    @param resource_name the resource name for atomizer selection
+    is ignored in this type of factory.
+    @return a StreamAtomizer object"""
+    return ByteStreamLineAtomizer(self.parsing_model, self.atom_handler_list, \
+                                  self.event_handler_list, 1 << 16, self.default_timestamp_paths)
