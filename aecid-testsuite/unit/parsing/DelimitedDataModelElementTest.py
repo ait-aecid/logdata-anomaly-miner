4741a0ae9d416114b5bf719f82ee6c47463d4d8e
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-05-17 08:48:32 +0200

    fixed indent issues in all parsing unittests.

--- aecid-testsuite/unit/parsing/DelimitedDataModelElementTest.py
+++ aecid-testsuite/unit/parsing/DelimitedDataModelElementTest.py
@@ -5,51 +5,44 @@ from aminer.parsing.MatchContext import MatchContext
 
 class DelimitedDataModelElementTest(unittest.TestCase):
     match_context_string = b'this is a match context'
-    '''
-    A single character is used as delimeter.
-    '''
-    def test1delimeter_single_char(self):
-      match_context = MatchContext(self.match_context_string)
-      delimited_data_model_element = DelimitedDataModelElement('id', b'c')
-      match_element = delimited_data_model_element.get_match_element('match', match_context)
-      self.assertEqual(match_element.get_match_string(), b'this is a mat')
-      
-      match_context = MatchContext(self.match_context_string)
-      delimited_data_model_element = DelimitedDataModelElement('id', b'f')
-      match_element = delimited_data_model_element.get_match_element('match', match_context)
-      self.assertEqual(match_element, None)
-      
-    '''
-    In this test case a whole string is searched for in the matchData.
-    '''
-    def test2delimeter_string(self):
-      match_context = MatchContext(self.match_context_string)
-      delimited_data_model_element = DelimitedDataModelElement('id', b' is')
-      match_element = delimited_data_model_element.get_match_element('match', match_context)
-      self.assertEqual(match_element.get_match_string(), b'this')
-    
-    '''
-    In this test case all not allowed values are tested.
-    '''
-    def test3delimeter_none_empty_or_not_printable(self):
-      match_context = MatchContext(self.match_context_string)
-      delimited_data_model_element = DelimitedDataModelElement('id', b'')
-      match_element = delimited_data_model_element.get_match_element('match', match_context)
-      self.assertEqual(match_element, None)
-      
-      match_context = MatchContext(self.match_context_string)
-      delimited_data_model_element = DelimitedDataModelElement('id', None)
-      self.assertRaises(TypeError, delimited_data_model_element.get_match_element, 'match', match_context)
-      
-      match_context = MatchContext(self.match_context_string)
-      delimited_data_model_element = DelimitedDataModelElement('id', b'\x01')
-      match_element = delimited_data_model_element.get_match_element('match', match_context)
-      self.assertEqual(match_element, None)
 
-    '''
-    In this test case special character escaping is tested.
-    '''
+    def test1delimeter_single_char(self):
+        """A single character is used as delimeter."""
+        match_context = MatchContext(self.match_context_string)
+        delimited_data_model_element = DelimitedDataModelElement('id', b'c')
+        match_element = delimited_data_model_element.get_match_element('match', match_context)
+        self.assertEqual(match_element.get_match_string(), b'this is a mat')
+
+        match_context = MatchContext(self.match_context_string)
+        delimited_data_model_element = DelimitedDataModelElement('id', b'f')
+        match_element = delimited_data_model_element.get_match_element('match', match_context)
+        self.assertEqual(match_element, None)
+
+    def test2delimeter_string(self):
+        """In this test case a whole string is searched for in the match_data."""
+        match_context = MatchContext(self.match_context_string)
+        delimited_data_model_element = DelimitedDataModelElement('id', b' is')
+        match_element = delimited_data_model_element.get_match_element('match', match_context)
+        self.assertEqual(match_element.get_match_string(), b'this')
+
+    def test3delimeter_none_empty_or_not_printable(self):
+        """In this test case all not allowed values are tested."""
+        match_context = MatchContext(self.match_context_string)
+        delimited_data_model_element = DelimitedDataModelElement('id', b'')
+        match_element = delimited_data_model_element.get_match_element('match', match_context)
+        self.assertEqual(match_element, None)
+
+        match_context = MatchContext(self.match_context_string)
+        delimited_data_model_element = DelimitedDataModelElement('id', None)
+        self.assertRaises(TypeError, delimited_data_model_element.get_match_element, 'match', match_context)
+
+        match_context = MatchContext(self.match_context_string)
+        delimited_data_model_element = DelimitedDataModelElement('id', b'\x01')
+        match_element = delimited_data_model_element.get_match_element('match', match_context)
+        self.assertEqual(match_element, None)
+
     def test4special_characters_escape(self):
+        """In this test case special character escaping is tested."""
         match_context = MatchContext(b'error: the command \\"python run.py\\" was not found" ')
         delimited_data_model_element = DelimitedDataModelElement('id', b'"', b'\\')
         match_element = delimited_data_model_element.get_match_element('match', match_context)
--- aecid-testsuite/unit/parsing/MatchElementTest.py
+++ aecid-testsuite/unit/parsing/MatchElementTest.py
@@ -4,46 +4,47 @@ from aminer.parsing.MatchElement import MatchElement
 
 class MatchElementTest(unittest.TestCase):
 
-    '''
-    This test case checks, whether a Exception is raised, when the path is None or empty and children are passed.
-    '''
     def test1_child_elements_with_no_path(self):
-      self.assertRaises(Exception, MatchElement, None, b'matchString', b'matchString', 
-          (MatchElement('child', b'childMatchString', b'childMatchString', [])))
-      
-      self.assertRaises(Exception, MatchElement, '', b'matchString', b'matchString', 
-          (MatchElement('child', b'childMatchString', b'childMatchString', [])))
-    
-    '''
-    This test case checks if all possible annotations are created correctly.
-    '''
+        """This test case checks, whether a Exception is raised, when the path is None or empty and children are passed."""
+        self.assertRaises(Exception, MatchElement, None, b'matchString', b'matchString',
+            (MatchElement('child', b'childMatchString', b'childMatchString', [])))
+
+        self.assertRaises(Exception, MatchElement, '', b'matchString', b'matchString',
+            (MatchElement('child', b'childMatchString', b'childMatchString', [])))
+
     def test2annotate_match(self):
-      a3 = MatchElement('a3', b'a3', b'a3', [])
-      a2 = MatchElement('a2', b'a2', b'a2', [a3])
-      a1 = MatchElement('a1', b'a1', b'a1', [a2])
-      b3 = MatchElement('b3', b'b3', b'b3', [])
-      b2 = MatchElement('b2', b'b2', b'b2', [b3])
-      b1 = MatchElement('b1', b'b1', b'b1', [b2])
-      
-      root_element = MatchElement('root', b'root', b'root', [a1, b1])
-      
-      self.assertEqual(root_element.annotate_match(None), "root: b'root' a1: b'a1' a2: b'a2' a3: b'a3' b1: b'b1' b2: b'b2' b3: b'b3'")
-      self.assertEqual(root_element.annotate_match(''), "root: b'root'\n  a1: b'a1'\n    a2: b'a2'\n      a3: b'a3'\n  b1: b'b1'\n    b2: b'b2'\n      b3: b'b3'")
-    
-    '''
-    This test case checks if all child objects are serialized correctly.
-    '''
+        """This test case checks if all possible annotations are created correctly."""
+        a3 = MatchElement('a3', b'a3', b'a3', [])
+        a2 = MatchElement('a2', b'a2', b'a2', [a3])
+        a1 = MatchElement('a1', b'a1', b'a1', [a2])
+        b3 = MatchElement('b3', b'b3', b'b3', [])
+        b2 = MatchElement('b2', b'b2', b'b2', [b3])
+        b1 = MatchElement('b1', b'b1', b'b1', [b2])
+
+        root_element = MatchElement('root', b'root', b'root', [a1, b1])
+
+        self.assertEqual(root_element.annotate_match(None), "root: b'root' a1: b'a1' a2: b'a2' a3: b'a3' b1: b'b1' b2: b'b2' b3: b'b3'")
+        self.assertEqual(root_element.annotate_match(''), "root: b'root'\n  a1: b'a1'\n    a2: b'a2'\n      a3: b'a3'\n  b1: b'b1'\n"
+                                                          "    b2: b'b2'\n      b3: b'b3'")
+
     def test3serialize_object(self):
-      a3 = MatchElement('a3', b'a3', b'a3', [])
-      a2 = MatchElement('a2', b'a2', b'a2', [a3])
-      a1 = MatchElement('a1', b'a1', b'a1', [a2])
-      b3 = MatchElement('b3', b'b3', b'b3', [])
-      b2 = MatchElement('b2', b'b2', b'b2', [b3])
-      b1 = MatchElement('b1', b'b1', b'b1', [b2])
-      
-      root_element = MatchElement('root', b'root', b'root', [a1, b1])
-      
-      self.assertEqual(root_element.serialize_object(), {'path': 'root', 'matchobject': b'root', 'matchString': b'root', 'children': [{'path': 'a1', 'matchobject': b'a1', 'matchString': b'a1', 'children': [{'path': 'a2', 'matchobject': b'a2', 'matchString': b'a2', 'children': [{'path': 'a3', 'matchobject': b'a3', 'matchString': b'a3', 'children': []}]}]}, {'path': 'b1', 'matchobject': b'b1', 'matchString': b'b1', 'children': [{'path': 'b2', 'matchobject': b'b2', 'matchString': b'b2', 'children': [{'path': 'b3', 'matchobject': b'b3', 'matchString': b'b3', 'children': []}]}]}]})
+        """This test case checks if all child objects are serialized correctly."""
+        a3 = MatchElement('a3', b'a3', b'a3', [])
+        a2 = MatchElement('a2', b'a2', b'a2', [a3])
+        a1 = MatchElement('a1', b'a1', b'a1', [a2])
+        b3 = MatchElement('b3', b'b3', b'b3', [])
+        b2 = MatchElement('b2', b'b2', b'b2', [b3])
+        b1 = MatchElement('b1', b'b1', b'b1', [b2])
+
+        root_element = MatchElement('root', b'root', b'root', [a1, b1])
+
+        self.assertEqual(root_element.serialize_object(), {'path': 'root', 'matchobject': b'root', 'matchString': b'root', 'children': [
+            {'path': 'a1', 'matchobject': b'a1', 'matchString': b'a1', 'children': [
+                {'path': 'a2', 'matchobject': b'a2', 'matchString': b'a2',
+                    'children': [{'path': 'a3', 'matchobject': b'a3', 'matchString': b'a3', 'children': []}]}]},
+            {'path': 'b1', 'matchobject': b'b1', 'matchString': b'b1', 'children': [
+                {'path': 'b2', 'matchobject': b'b2', 'matchString': b'b2',
+                    'children': [{'path': 'b3', 'matchobject': b'b3', 'matchString': b'b3', 'children': []}]}]}]})
 
 
 if __name__ == "__main__":
