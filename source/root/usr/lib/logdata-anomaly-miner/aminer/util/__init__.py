a7d1cbef53ab8568ba3e398c2419e9455c626ba4
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-05-26 17:00:02 +0200

    test

--- source/root/usr/lib/logdata-anomaly-miner/aminer/util/__init__.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/util/__init__.py
@@ -21,11 +21,12 @@ def get_log_int(max_bits):
 def decode_string_as_byte_string(string):
     """Decodes a string produced by the encode function encodeByteStringAsString(byteString) below.
     @return string."""
+    return string.decode()
     decoded = b''
     count = 0
     while count < len(string):
         if string[count] in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!"#$&\'()*+,-./:;<=>?@[]\\^_`{}|~ ':
-            decoded += bytes(string[count], 'utf-8')
+            decoded += bytes(string[count], 'ascii')
             count += 1
         elif string[count] == '%':
             decoded += bytearray((int(string[count + 1:count + 3], 16),))
@@ -39,6 +40,7 @@ def encode_byte_string_as_string(byte_string):
     r"""Encodes an arbitrary byte string to a string by replacing all non ascii-7 bytes and all non printable ascii-7 bytes
     and % character by replacing with their escape sequence %[hex]. For example byte string b'/\xc3' is encoded to '/%c3'
     @return a string with decoded name."""
+    return byte_string.encode()
     encoded = ''
     for byte in byte_string:
         if byte in b'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!"#$&\'()*+,-./:;<=>?@[]\\^_`{}|~ ':
