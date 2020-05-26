be532e8a54d8afec028ad18ce0001dfe3c700c41
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-05-26 19:43:39 +0200

    test

--- source/root/usr/lib/logdata-anomaly-miner/aminer/util/PersistencyUtil.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/util/PersistencyUtil.py
@@ -102,6 +102,6 @@ def store_json(file_name, object_data):
     """Store persistency data to file."""
     persistence_data = JsonUtil.dump_as_json(object_data)
     fd = create_temporary_persistence_file(file_name)
-    os.write(fd, bytes(persistence_data))
+    os.write(fd, bytes(persistence_data, 'utf-8'))
     replace_persistence_file(file_name, fd)
     os.close(fd)
