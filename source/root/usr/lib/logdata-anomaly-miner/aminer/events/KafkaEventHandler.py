2b3fd71287b9bd976eb43202f65f2f6166b00897
max.landauer@ait.ac.at <debian@mail.hieroglyph.com>
2020-05-28 11:18:08 +0000

    Fix kafka module import

--- source/root/usr/lib/logdata-anomaly-miner/aminer/events/KafkaEventHandler.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/events/KafkaEventHandler.py
@@ -13,9 +13,18 @@ class KafkaEventHandler(EventHandlerInterface):
         self.analysis_context = analysis_context
         self.producer = KafkaProducer(**options, value_serializer=lambda v: v.encode())
         self.topic = topic
+        self.kafkaImported = False
 
     def receive_event(self, event_type, event_message, sorted_log_lines, event_data, log_atom, event_source):
         """Receive information about a detected event in json format"""
+        if self.kafkaImported is False:
+            try:
+                from kafka import KafkaProducer
+                from kafka.errors import KafkaError
+                self.kafkaImported = True
+            except ImportError as error:
+                print('ERROR: Kafka module not found.', file=sys.stderr)
+                return False
         if not isinstance(event_data, str) and not isinstance(event_data, bytes):
             print('WARNING: KafkaEventHandler received non-string event data. Use the JsonConverterHandler to serialize it first.',
                   file=sys.stderr)
