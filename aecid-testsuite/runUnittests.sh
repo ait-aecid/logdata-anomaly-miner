6192ef379c93df9329dbab559ecfab404472cd16
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-06-03 14:21:49 +0200

    added KafkaEventHandlerTest and fixed minor error in KafkaEventHandler.

--- aecid-testsuite/runUnittests.sh
+++ aecid-testsuite/runUnittests.sh
@@ -1,6 +1,16 @@
+curl https://mirror.klaus-uwe.me/apache/kafka/2.5.0/kafka_2.12-2.5.0.tgz --output kafka.tgz
+tar xvf kafka.tgz
+rm kafka.tgz
+bin/zookeeper-server-start.sh config/zookeeper.properties &
+sleep 1
+bin/kafka-server-start.sh config/server.properties &
+
 sudo python3 -m unittest discover -s unit -p '*Test.py' > /dev/null
 test -e /var/mail/mail && sudo rm -f /var/mail/mail
 sudo rm /tmp/AMinerRemoteLog.txt
 sudo rm /tmp/test4unixSocket.sock
 sudo rm /tmp/test5unixSocket.sock
 sudo rm /tmp/test6unixSocket.sock
+
+bin/kafka-server-stop.sh
+bin/zookeeper-server-stop.sh
