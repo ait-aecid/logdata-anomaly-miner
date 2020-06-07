cf539f13f2f67324a02e5c7052f22e8006816736
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-06-07 20:47:11 +0200

    test

--- aecid-testsuite/runUnittests.sh
+++ aecid-testsuite/runUnittests.sh
@@ -2,9 +2,9 @@ curl https://mirror.klaus-uwe.me/apache/kafka/2.5.0/kafka_2.12-2.5.0.tgz --outpu
 tar xvf kafka.tgz > /dev/null
 rm kafka.tgz
 
-kafka_2.12-2.5.0/bin/zookeeper-server-start.sh kafka_2.12-2.5.0/config/zookeeper.properties > /dev/null &
+kafka_2.12-2.5.0/bin/zookeeper-server-start.sh kafka_2.12-2.5.0/config/zookeeper.properties #> /dev/null &
 sleep 1
-kafka_2.12-2.5.0/bin/kafka-server-start.sh kafka_2.12-2.5.0/config/server.properties > /dev/null &
+kafka_2.12-2.5.0/bin/kafka-server-start.sh kafka_2.12-2.5.0/config/server.properties #> /dev/null &
 
 sudo python3 -m unittest discover -s unit -p '*Test.py' > /dev/null
 test -e /var/mail/mail && sudo rm -f /var/mail/mail
@@ -13,6 +13,6 @@ sudo rm /tmp/test4unixSocket.sock
 sudo rm /tmp/test5unixSocket.sock
 sudo rm /tmp/test6unixSocket.sock
 
-kafka_2.12-2.5.0/bin/kafka-server-stop.sh > /dev/null
-kafka_2.12-2.5.0/bin/zookeeper-server-stop.sh > /dev/null
+kafka_2.12-2.5.0/bin/kafka-server-stop.sh #> /dev/null
+kafka_2.12-2.5.0/bin/zookeeper-server-stop.sh #> /dev/null
 sudo rm -r kafka_2.12-2.5.0/
