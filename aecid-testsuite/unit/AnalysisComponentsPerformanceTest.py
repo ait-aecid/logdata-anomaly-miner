a8d14ba039c0a3a9027879887622e8340a84e3b7
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-07-24 14:39:29 +0200

    fixed style issues.

--- aecid-testsuite/unit/AnalysisComponentsPerformanceTest.py
+++ aecid-testsuite/unit/AnalysisComponentsPerformanceTest.py
@@ -422,9 +422,8 @@ class AnalysisComponentsPerformanceTest(TestBase):
         avg = 0
         z = 0
         while z < self.iterations:
-            time_correlation_detector = TimeCorrelationDetector(self.aminer_config, [self.stream_printer_event_handler],
-                                                                number_of_rules, 'Default', self.waiting_time * 9000,
-                                                                True, True, True, 1, 5)
+            time_correlation_detector = TimeCorrelationDetector(self.aminer_config, [self.stream_printer_event_handler], number_of_rules,
+                                                                'Default', self.waiting_time * 9000, True, True, True, 1, 5)
             t = time.time()
             measured_time = 0
             i = 0
