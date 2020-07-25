a9f42b1be6e339da033ec3c4d96c2d7f33e2d1f4
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-07-25 09:48:47 +0200

    fixed exit codes of travis run-scripts.

--- aecid-testsuite/runAMinerDemo.sh
+++ aecid-testsuite/runAMinerDemo.sh
@@ -3,7 +3,9 @@ sudo chown aminer:aminer /tmp/demo-config.py
 sudo chown -R aminer:aminer /tmp/lib
 sudo chmod +x demo/AMiner/aminerDemo.sh
 sudo -u aminer ./demo/AMiner/aminerDemo.sh > /tmp/demo
+exit_code=$?
 sudo rm /tmp/demo-config.py
 sudo rm /tmp/demo
 sudo rm /tmp/syslog
 sudo rm /tmp/AMinerRemoteLog.txt
+exit $exit_code
