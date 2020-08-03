1ccba597e0e68a096d6814bd61ff0b23f9382a3c
root <max.landauer@ait.ac.at>
2020-08-03 08:02:40 +0000

    Fix deepsource issues

--- source/root/etc/aminer/conf-available/ait-lds/ApacheAccessParsingModel.py
+++ source/root/etc/aminer/conf-available/ait-lds/ApacheAccessParsingModel.py
@@ -9,42 +9,43 @@ from aminer.parsing import DelimitedDataModelElement
 from aminer.parsing import FirstMatchModelElement
 from aminer.parsing import FixedDataModelElement
 from aminer.parsing import FixedWordlistDataModelElement
-from aminer.parsing import HexStringModelElement
 from aminer.parsing import IpAddressDataModelElement
 from aminer.parsing import OptionalMatchModelElement
 from aminer.parsing import SequenceModelElement
 from aminer.parsing import VariableByteDataModelElement
 
 def getModel():
-	dict = b'!"#$%&\'()*+,-./0123456789:;<>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ\\^_`abcdefghijklmnopqrstuvwxyz{|}~=[]'
+    """This model defines how to parse Apache Access logs from the AIT-LDS."""
 
-	model = SequenceModelElement('model', [
-		FirstMatchModelElement('client_ip', [
-			IpAddressDataModelElement('client_ip'),
-			FixedDataModelElement('localhost', b'::1')
-			]),
-		FixedDataModelElement('sp1', b' '),
-		VariableByteDataModelElement('client_id', dict),
-		FixedDataModelElement('sp2', b' '),
-		VariableByteDataModelElement('user_id', dict),
-		FixedDataModelElement('sp3', b' ['),
-		DateTimeModelElement('time', b'%d/%b/%Y:%H:%M:%S'),
-		FixedDataModelElement('sp4', b' +'),
-		DecimalIntegerValueModelElement('tz'),
-		FixedDataModelElement('sp5', b'] "'),
-		DelimitedDataModelElement('request', b'"', b'\\'),
-		FixedDataModelElement('sp6', b'" '),
-		DecimalIntegerValueModelElement('status_code'),
-		FixedDataModelElement('sp7', b' '),
-		DecimalIntegerValueModelElement('content_size'),
-		OptionalMatchModelElement('combined', 
-			SequenceModelElement('combined', [
-				FixedDataModelElement('sp8', b' "'),
-				DelimitedDataModelElement('referer', b'"', b'\\'),
-				FixedDataModelElement('sp9', b'" "'),
-				DelimitedDataModelElement('user_agent', b'"', b'\\'),
-				FixedDataModelElement('sp10', b'"'),
-				])),
-		])
+    alphabet = b'!"#$%&\'()*+,-./0123456789:;<>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ\\^_`abcdefghijklmnopqrstuvwxyz{|}~=[]'
 
-	return model
+    model = SequenceModelElement('model', [
+        FirstMatchModelElement('client_ip', [
+            IpAddressDataModelElement('client_ip'),
+            FixedDataModelElement('localhost', b'::1')
+            ]),
+        FixedDataModelElement('sp1', b' '),
+        VariableByteDataModelElement('client_id', alphabet),
+        FixedDataModelElement('sp2', b' '),
+        VariableByteDataModelElement('user_id', alphabet),
+        FixedDataModelElement('sp3', b' ['),
+        DateTimeModelElement('time', b'%d/%b/%Y:%H:%M:%S'),
+        FixedDataModelElement('sp4', b' +'),
+        DecimalIntegerValueModelElement('tz'),
+        FixedDataModelElement('sp5', b'] "'),
+        DelimitedDataModelElement('request', b'"', b'\\'),
+        FixedDataModelElement('sp6', b'" '),
+        DecimalIntegerValueModelElement('status_code'),
+        FixedDataModelElement('sp7', b' '),
+        DecimalIntegerValueModelElement('content_size'),
+        OptionalMatchModelElement('combined',
+            SequenceModelElement('combined', [
+                FixedDataModelElement('sp8', b' "'),
+                DelimitedDataModelElement('referer', b'"', b'\\'),
+                FixedDataModelElement('sp9', b'" "'),
+                DelimitedDataModelElement('user_agent', b'"', b'\\'),
+                FixedDataModelElement('sp10', b'"'),
+                ])),
+        ])
+
+    return model
