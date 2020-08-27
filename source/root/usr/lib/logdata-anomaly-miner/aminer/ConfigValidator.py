c637ce53cb9267d97f7c1b9dc640ee2fd7499ed6
Wolfgang Hotwagner <wolfgang.hotwagner@ait.ac.at>
2020-08-27 08:19:41 +0000

    Added parser-validation for the yaml-config
    This commit adds a new validation rule for the yaml-schema that checks
    if the parser can be imported.

--- source/root/usr/lib/logdata-anomaly-miner/aminer/ConfigValidator.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/ConfigValidator.py
@@ -1,6 +1,33 @@
 from cerberus import Validator
+from cerberus import TypeDefinition
+
+class ParserModelType:
+    name = None
+    ismodel = False
+    func = None
+
+    def __init__(self,name):
+        self.name = name
+        if name.endswith('ModelElement'):
+            self.ismodel = True
+            self.func = getattr(__import__("aminer.parsing", fromlist=[name]), name)
+        else:
+            self.ismodel = False
+            self.func = getattr(__import__(name), 'get_model')
+
+    def __str__(self):
+        return self.name
+
+parser_type = TypeDefinition('parsermodel', (ParserModelType,str), ())
 
 class ConfigValidator(Validator):
+    types_mapping = Validator.types_mapping.copy()
+    types_mapping['parsermodel'] = parser_type
+
+    def _normalize_coerce_toparsermodel(self,value):
+        if isinstance(value, str):
+            return ParserModelType(value)
+
     def _validate_has_start(self, has_start, field, value):
         seen_start = False
         for var in value:
