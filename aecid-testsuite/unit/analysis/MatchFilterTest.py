f03f76d236957163d906b282ea0da184c22a9c37
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-09-02 07:48:15 +0200

    removed randomness in MatchFilterTest.

--- aecid-testsuite/unit/analysis/MatchFilterTest.py
+++ aecid-testsuite/unit/analysis/MatchFilterTest.py
@@ -1,5 +1,4 @@
 import unittest
-import random
 import time
 from unit.TestBase import TestBase
 from aminer.analysis.MatchFilter import MatchFilter
@@ -20,8 +19,7 @@ class MatchFilterTest(TestBase):
         match_filter = MatchFilter(self.aminer_config, ['/integer'], [self.stream_printer_event_handler])
         self.analysis_context.register_component(match_filter, description)
         t = time.time()
-        for _ in range(1000):
-            val = random.randint(0, 1000)
+        for val in range(1000):
             log_atom = LogAtom(val, ParserMatch(decimal_integer_me.get_match_element('', MatchContext(str(val).encode('utf-8')))), t,
                                match_filter)
             match_filter.receive_atom(log_atom)
@@ -36,8 +34,7 @@ class MatchFilterTest(TestBase):
         match_filter = MatchFilter(self.aminer_config, ['/strings'], [self.stream_printer_event_handler])
         self.analysis_context.register_component(match_filter, description)
         t = time.time()
-        for _ in range(1000):
-            val = random.randint(0, 1000)
+        for val in range(1000):
             log_atom = LogAtom(val, ParserMatch(decimal_integer_me.get_match_element('', MatchContext(str(val).encode('utf-8')))), t,
                                match_filter)
             match_filter.receive_atom(log_atom)
@@ -51,8 +48,7 @@ class MatchFilterTest(TestBase):
             range(1001)))
         self.analysis_context.register_component(match_filter, description)
         t = time.time()
-        for _ in range(1000):
-            val = random.randint(0, 1000)
+        for val in range(1000):
             log_atom = LogAtom(val, ParserMatch(decimal_integer_me.get_match_element('', MatchContext(str(val).encode('utf-8')))), t,
                                match_filter)
             match_filter.receive_atom(log_atom)
@@ -69,8 +65,7 @@ class MatchFilterTest(TestBase):
             range(501)))
         self.analysis_context.register_component(match_filter, description)
         t = time.time()
-        for _ in range(1000):
-            val = random.randint(0, 1000)
+        for val in range(1000):
             log_atom = LogAtom(val, ParserMatch(decimal_integer_me.get_match_element('', MatchContext(str(val).encode('utf-8')))), t,
                                match_filter)
             match_filter.receive_atom(log_atom)
