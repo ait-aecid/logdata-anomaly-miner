c3bd0712c431cad3c54185ce1625ec105d0110d1
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-09-29 16:37:49 +0200

    updated yaml tests.

--- aecid-testsuite/unit/config/YamlConfigTest.py
+++ aecid-testsuite/unit/config/YamlConfigTest.py
@@ -18,13 +18,13 @@ class YamlConfigTest(unittest.TestCase):
     def tearDown(self):
         sys.path = self.sysp
 
-    """ Loads a yaml file into the variable aminer_config.yamldata """
+    """ Loads a yaml file into the variable aminer_config.yaml_data """
     def test_load_generic_yaml_file(self):
         spec = importlib.util.spec_from_file_location('aminer_config', '/usr/lib/logdata-anomaly-miner/aminer/ymlconfig.py')
         aminer_config = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(aminer_config)
-        aminer_config.loadYaml('unit/data/configfiles/template_config.yml')
-        self.assertIsNotNone(aminer_config.yamldata)
+        aminer_config.load_yaml('unit/data/configfiles/template_config.yml')
+        self.assertIsNotNone(aminer_config.yaml_data)
 
     """ Tries to load a nonexistent yaml file. a FileNotFoundError is expected """
     def test_load_notexistent_yaml_file(self):
@@ -32,7 +32,7 @@ class YamlConfigTest(unittest.TestCase):
         aminer_config = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(aminer_config)
         with self.assertRaises(FileNotFoundError):
-            aminer_config.loadYaml('unit/data/configfiles/doesnotexist.yml')
+            aminer_config.load_yaml('unit/data/configfiles/doesnotexist.yml')
 
     """ Tries to load a file with invalid yaml syntax. Expects an YAMLError """
     def test_load_invalid_yaml_file(self):
@@ -40,7 +40,7 @@ class YamlConfigTest(unittest.TestCase):
         aminer_config = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(aminer_config)
         with self.assertRaises(yaml.YAMLError):
-            aminer_config.loadYaml('unit/data/configfiles/invalid_config.yml')
+            aminer_config.load_yaml('unit/data/configfiles/invalid_config.yml')
 
     """ Tries to load a yaml-file with an invalid schema. A ValueError is expected """
     def test_load_yaml_file_with_invalid_schema(self):
@@ -48,14 +48,14 @@ class YamlConfigTest(unittest.TestCase):
         aminer_config = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(aminer_config)
         with self.assertRaises(ValueError):
-            aminer_config.loadYaml('unit/data/configfiles/invalid_schema.yml')
+            aminer_config.load_yaml('unit/data/configfiles/invalid_schema.yml')
 
     """ This test builds a analysis_pipeline from a valid yaml-file """
     def test_analysis_pipeline_working_config(self):
         spec = importlib.util.spec_from_file_location('aminer_config', '/usr/lib/logdata-anomaly-miner/aminer/ymlconfig.py')
         aminer_config = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(aminer_config)
-        aminer_config.loadYaml('unit/data/configfiles/apache_inline_config.yml')
+        aminer_config.load_yaml('unit/data/configfiles/apache_inline_config.yml')
         from aminer.AnalysisChild import AnalysisContext
         context = AnalysisContext(aminer_config)
         context.build_analysis_pipeline()
@@ -129,7 +129,7 @@ class YamlConfigTest(unittest.TestCase):
         aminer_config = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(aminer_config)
         with self.assertRaises(ValueError):
-            aminer_config.loadYaml('unit/data/configfiles/missing_parserstart_config.yml')
+            aminer_config.load_yaml('unit/data/configfiles/missing_parserstart_config.yml')
 
     """ This test checks if the aminer fails without a start-tag for the first parser-model """
     def test_analysis_fail_with_double_parser_start(self):
@@ -137,7 +137,7 @@ class YamlConfigTest(unittest.TestCase):
         aminer_config = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(aminer_config)
         with self.assertRaises(ValueError):
-            aminer_config.loadYaml('unit/data/configfiles/double_parserstart_config.yml')
+            aminer_config.load_yaml('unit/data/configfiles/double_parserstart_config.yml')
 
     """ This test checks if the config-schema-validator raises an error if an unknown parser is configured  """
     def test_analysis_fail_with_unknown_parser_start(self):
@@ -145,14 +145,14 @@ class YamlConfigTest(unittest.TestCase):
         aminer_config = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(aminer_config)
         with self.assertRaises(ValueError):
-            aminer_config.loadYaml('unit/data/configfiles/unknown_parser_config.yml')
+            aminer_config.load_yaml('unit/data/configfiles/unknown_parser_config.yml')
 
     """This test checks if the config can be loaded without any analysis components."""
     def test_analysis_pipeline_working_config_with_analysis_components(self):
         spec = importlib.util.spec_from_file_location('aminer_config', '/usr/lib/logdata-anomaly-miner/aminer/ymlconfig.py')
         aminer_config = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(aminer_config)
-        aminer_config.loadYaml('unit/data/configfiles/apache_inline_config_null_analysis_components.yml')
+        aminer_config.load_yaml('unit/data/configfiles/apache_inline_config_null_analysis_components.yml')
         from aminer.AnalysisChild import AnalysisContext
         context = AnalysisContext(aminer_config)
         context.build_analysis_pipeline()
@@ -161,7 +161,7 @@ class YamlConfigTest(unittest.TestCase):
         spec = importlib.util.spec_from_file_location('aminer_config', '/usr/lib/logdata-anomaly-miner/aminer/ymlconfig.py')
         aminer_config = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(aminer_config)
-        aminer_config.loadYaml('unit/data/configfiles/apache_inline_config_undefined_analysis_components.yml')
+        aminer_config.load_yaml('unit/data/configfiles/apache_inline_config_undefined_analysis_components.yml')
         context = AnalysisContext(aminer_config)
         context.build_analysis_pipeline()
         self.run_empty_analysis_components_tests(context)
