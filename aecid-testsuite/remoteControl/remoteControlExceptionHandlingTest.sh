bbf347c598cbbe86eded1078f2f4f25d8e8d1d9d
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-10-14 19:32:40 +0200

    fixed errors in the remoteControlExceptionHandlingTest.

--- aecid-testsuite/remoteControl/remoteControlExceptionHandlingTest.sh
+++ aecid-testsuite/remoteControl/remoteControlExceptionHandlingTest.sh
@@ -1,56 +1,56 @@
 #This test can be run with the existing demo-config of the aminerRemoteControl module.
 
 echo "removes the 'LogPrefix'."
-sudo aminerRemoteControl --Exec "changeConfigProperty(analysisContext, 'LogPrefix', '')"
+sudo aminerRemoteControl --Exec "change_config_property(analysis_context, 'LogPrefix', '')"
 echo
 
 echo "removes the 'LogPrefix' wrong Type."
-sudo aminerRemoteControl --Exec "changeConfigProperty(analysisContext, 'LogPrefix', None)"
+sudo aminerRemoteControl --Exec "change_config_property(analysis_context, 'LogPrefix', None)"
 echo
 
 echo "renames the 'NewMatchPathValueCombo' component to 'NewMatchPathValueComboDetector'."
-sudo aminerRemoteControl --Exec "renameRegisteredAnalysisComponent(analysisContext,'NewMatchPathValueCombo','NewMatchPathValueComboDetector')"
+sudo aminerRemoteControl --Exec "rename_registered_analysis_component(analysis_context,'NewMatchPathValueCombo','NewMatchPathValueComboDetector')"
 echo
 
 echo "renames the 'NewMatchPathValueComboDetector' wrong Type. (no string; integer value)"
-sudo aminerRemoteControl --Exec "renameRegisteredAnalysisComponent(analysisContext,'NewMatchPathValueComboDetector', 222)"
+sudo aminerRemoteControl --Exec "rename_registered_analysis_component(analysis_context,'NewMatchPathValueComboDetector', 222)"
 echo
 
 echo "renames a non existing component to 'NewMatchPathValueComboDetector'."
-sudo aminerRemoteControl --Exec "renameRegisteredAnalysisComponent(analysisContext,'NonExistingDetector','NewMatchPathValueComboDetector')"
+sudo aminerRemoteControl --Exec "rename_registered_analysis_component(analysis_context,'NonExistingDetector','NewMatchPathValueComboDetector')"
 echo
 
-echo "changes the 'autoIncludeFlag' of the 'NewMatchPathValueComboDetector' to False."
-sudo aminerRemoteControl --Exec "changeAttributeOfRegisteredAnalysisComponent(analysisContext, 'NewMatchPathValueComboDetector',  'auto_include_flag', False)"
+echo "changes the 'auto_include_flag' of the 'NewMatchPathValueComboDetector' to False."
+sudo aminerRemoteControl --Exec "change_attribute_of_registered_analysis_component(analysis_context, 'NewMatchPathValueComboDetector',  'auto_include_flag', False)"
 echo
 
 echo "changes the 'auto_include_flag' of the 'NewMatchPathValueComboDetector' wrong Type."
-sudo aminerRemoteControl --Exec "changeAttributeOfRegisteredAnalysisComponent(analysisContext, 'NewMatchPathValueComboDetector',  'auto_include_flag', 'True')"
+sudo aminerRemoteControl --Exec "change_attribute_of_registered_analysis_component(analysis_context, 'NewMatchPathValueComboDetector',  'auto_include_flag', 'True')"
 echo
 
 echo "prints the current list of paths."
-sudo aminerRemoteControl --Exec "printAttributeOfRegisteredAnalysisComponent(analysisContext, 'NewMatchPathValueComboDetector',  'target_path_list')"
+sudo aminerRemoteControl --Exec "print_attribute_of_registered_analysis-component(analysis_context, 'NewMatchPathValueComboDetector',  'target_path_list')"
 echo
 
 echo "prints not existing attribute."
-sudo aminerRemoteControl --Exec "printAttributeOfRegisteredAnalysisComponent(analysisContext, 'NewMatchPathValueComboDetector',  'otherPathList')"
+sudo aminerRemoteControl --Exec "print_attribute_of_registered_analysis-component(analysis_context, 'NewMatchPathValueComboDetector',  'other_path_list')"
 echo
 
 echo "add a new NewMatchPathDetector to the config."
-sudo aminerRemoteControl --Exec "addHandlerToAtomFilterAndRegisterAnalysisComponent(analysisContext, 'AtomFilter', NewMatchPathDetector(analysisContext.aminer_config, analysisContext.atomizer_factory.atom_handler_list, auto_include_flag=True), 'NewMatchPathDet')"
+sudo aminerRemoteControl --Exec "add_handler_to_atom_filter_and_register_analysis_component(analysis_context, 'AtomFilter', NewMatchPathDetector(analysis_context.aminer_config, analysis_context.atomizer_factory.atom_handler_list, auto_include_flag=True), 'NewMatchPathDet')"
 echo
 
 echo "add a wrong class to the config."
-sudo aminerRemoteControl --Exec "addHandlerToAtomFilterAndRegisterAnalysisComponent(analysisContext, 'AtomFilter', 'StringDetector', 'StringDetector')"
+sudo aminerRemoteControl --Exec "add_handler_to_atom_filter_and_register_analysis_component(analysis_context, 'AtomFilter', 'StringDetector', 'StringDetector')"
 echo
 
 echo "save the current config to /tmp/config.py"
-sudo aminerRemoteControl --Exec "saveCurrentConfig(analysisContext,'/tmp/config.py')"
+sudo aminerRemoteControl --Exec "save_current_config(analysis_context,'/tmp/config.py')"
 echo
 
 echo "save the current config to an invalid path."
-sudo aminerRemoteControl --Exec "saveCurrentConfig(analysisContext,'[dd/path/config.py')"
+sudo aminerRemoteControl --Exec "save_current_config(analysis_context,'[dd/path/config.py')"
 echo
 
 echo "save the current config to an not existing directory path."
-sudo aminerRemoteControl --Exec "saveCurrentConfig(analysisContext,'/notExistingPath/config.py')"
+sudo aminerRemoteControl --Exec "save_current_config(analysis_context,'/notExistingPath/config.py')"
