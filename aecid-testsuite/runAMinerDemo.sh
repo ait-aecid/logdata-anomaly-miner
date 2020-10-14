9fbf6f91faa2ce135e9564f305d5801b0e2a6e65
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-10-14 14:42:30 +0200

    removed sudo from starting the aminerDemo.

--- aecid-testsuite/runAMinerDemo.sh
+++ aecid-testsuite/runAMinerDemo.sh
@@ -2,7 +2,7 @@ cp $1 /tmp/demo-config.py
 sudo chown aminer:aminer /tmp/demo-config.py 2> /dev/null
 sudo chown -R aminer:aminer /tmp/lib 2> /dev/null
 sudo chmod +x demo/AMiner/aminerDemo.sh
-sudo ./demo/AMiner/aminerDemo.sh > /tmp/demo
+./demo/AMiner/aminerDemo.sh > /tmp/demo
 exit_code=$?
 sudo rm /tmp/demo-config.py
 sudo rm /tmp/demo
