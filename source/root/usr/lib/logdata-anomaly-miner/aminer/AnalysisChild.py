f8cedf37e0ff97160468ef089394c212a3961e88
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-10-29 19:17:41 +0100

    no persistence is written when aminer is suspended.

--- source/root/usr/lib/logdata-anomaly-miner/aminer/AnalysisChild.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/AnalysisChild.py
@@ -357,8 +357,9 @@ class AnalysisChild(TimeTriggeredComponentInterface):
             if next_real_time_trigger_time is None or real_time >= next_real_time_trigger_time:
                 next_trigger_offset = 3600
                 for component in real_time_triggered_components:
-                    next_trigger_request = component.do_timer(real_time)
-                    next_trigger_offset = min(next_trigger_offset, next_trigger_request)
+                    if not suspended_flag:
+                        next_trigger_request = component.do_timer(real_time)
+                        next_trigger_offset = min(next_trigger_offset, next_trigger_request)
                 next_real_time_trigger_time = real_time + next_trigger_offset
 
             # Handle the analysis time events. The analysis time will be different when an analysis time component is registered.
@@ -368,8 +369,9 @@ class AnalysisChild(TimeTriggeredComponentInterface):
             if next_analysis_time_trigger_time is None or analysis_time >= next_analysis_time_trigger_time:
                 next_trigger_offset = 3600
                 for component in analysis_time_triggered_components:
-                    next_trigger_request = component.do_timer(real_time)
-                    next_trigger_offset = min(next_trigger_offset, next_trigger_request)
+                    if not suspended_flag:
+                        next_trigger_request = component.do_timer(real_time)
+                        next_trigger_offset = min(next_trigger_offset, next_trigger_request)
                 next_analysis_time_trigger_time = analysis_time + next_trigger_offset
 
         # Analysis loop is only left on shutdown. Try to persist everything and leave.
