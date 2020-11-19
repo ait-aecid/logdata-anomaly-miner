9ac6e45d93a47b516fc0333fe4c51081ffff493f
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-11-19 09:25:28 +0100

    initialized loggers in unittests so no error messages are printed unintentionally.

--- aecid-testsuite/unit/TestBase.py
+++ aecid-testsuite/unit/TestBase.py
@@ -8,6 +8,58 @@ from aminer.events.StreamPrinterEventHandler import StreamPrinterEventHandler
 from _io import StringIO
 
 
+def initialize_loggers(aminer_config, aminer_user, aminer_grp):
+    """Initialize all loggers."""
+    from aminer import AMinerConfig
+    datefmt = '%d/%b/%Y:%H:%M:%S %z'
+
+    persistence_dir = aminer_config.config_properties.get(AMinerConfig.KEY_PERSISTENCE_DIR, AMinerConfig.DEFAULT_PERSISTENCE_DIR)
+    rc_logger = logging.getLogger(AMinerConfig.REMOTE_CONTROL_LOG_NAME)
+    rc_logger.setLevel(logging.DEBUG)
+    remote_control_log_file = aminer_config.config_properties.get(
+        AMinerConfig.KEY_REMOTE_CONTROL_LOG_FILE, os.path.join(persistence_dir, AMinerConfig.DEFAULT_REMOTE_CONTROL_LOG_FILE))
+    try:
+        rc_file_handler = logging.FileHandler(remote_control_log_file)
+        shutil.chown(remote_control_log_file, aminer_user, aminer_grp)
+    except OSError as e:
+        print('Could not create or open %s: %s. Stopping..' % (remote_control_log_file, e), file=sys.stderr)
+        sys.exit(1)
+    rc_file_handler.setFormatter(logging.Formatter(fmt='%(asctime)s %(levelname)s %(message)s', datefmt=datefmt))
+    rc_logger.addHandler(rc_file_handler)
+    logging.addLevelName(15, "REMOTECONTROL")
+
+    stat_logger = logging.getLogger(AMinerConfig.STAT_LOG_NAME)
+    stat_logger.setLevel(logging.INFO)
+    stat_log_file = aminer_config.config_properties.get(
+        AMinerConfig.KEY_STAT_LOG_FILE, os.path.join(persistence_dir, AMinerConfig.DEFAULT_STAT_LOG_FILE))
+    try:
+        stat_file_handler = logging.FileHandler(stat_log_file)
+        shutil.chown(stat_log_file, aminer_user, aminer_grp)
+    except OSError as e:
+        print('Could not create or open %s: %s. Stopping..' % (stat_log_file, e), file=sys.stderr)
+        sys.exit(1)
+    stat_file_handler.setFormatter(logging.Formatter(fmt='%(asctime)s %(message)s', datefmt=datefmt))
+    stat_logger.addHandler(stat_file_handler)
+
+    debug_logger = logging.getLogger(AMinerConfig.DEBUG_LOG_NAME)
+    if AMinerConfig.DEBUG_LEVEL == 0:
+        debug_logger.setLevel(logging.ERROR)
+    elif AMinerConfig.DEBUG_LEVEL == 1:
+        debug_logger.setLevel(logging.INFO)
+    else:
+        debug_logger.setLevel(logging.DEBUG)
+    debug_log_file = aminer_config.config_properties.get(
+        AMinerConfig.KEY_DEBUG_LOG_FILE, os.path.join(persistence_dir, AMinerConfig.DEFAULT_DEBUG_LOG_FILE))
+    try:
+        debug_file_handler = logging.FileHandler(debug_log_file)
+        shutil.chown(debug_log_file, aminer_user, aminer_grp)
+    except OSError as e:
+        print('Could not create or open %s: %s. Stopping..' % (debug_log_file, e), file=sys.stderr)
+        sys.exit(1)
+    debug_file_handler.setFormatter(logging.Formatter(fmt='%(asctime)s %(levelname)s %(message)s', datefmt=datefmt))
+    debug_logger.addHandler(debug_file_handler)
+
+
 class TestBase(unittest.TestCase):
     """This is the base class for all unittests."""
 
@@ -24,7 +76,7 @@ class TestBase(unittest.TestCase):
             shutil.rmtree(persistence_file_name)
         if not os.path.exists(persistence_file_name):
             os.makedirs(persistence_file_name)
-        logging.disable(logging.CRITICAL)
+        initialize_loggers(self.aminer_config, 'aminer', 'aminer')
 
     def tearDown(self):
         """Delete all persisted data after the tests."""
@@ -34,7 +86,6 @@ class TestBase(unittest.TestCase):
             shutil.rmtree(persistence_file_name)
         if not os.path.exists(persistence_file_name):
             os.makedirs(persistence_file_name)
-        logging.disable(logging.NOTSET)
 
     def reset_output_stream(self):
         """Reset the output stream."""
