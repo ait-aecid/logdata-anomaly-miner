5d86e07953b22a1fd87bb8ef973c2640c32c7110
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-11-23 11:30:40 +0100

    added missing links.

--- aecid-testsuite/runGettingStarted.sh
+++ aecid-testsuite/runGettingStarted.sh
@@ -1,5 +1,5 @@
 sudo chown -R aminer:aminer /var/lib/aminer 2> /dev/null
-sudo aminer --Config demo/AMiner/gettingStarted-config.yml &
+sudo aminer --Config demo/AMiner/gettingStarted-config.yml > /dev/null &
 sleep 5 & wait $!
 sudo pkill -x aminer
 exit_code=$?
