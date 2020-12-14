cf990666c719323ae81bf784eded83457a02e080
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-12-14 13:41:14 +0100

    fixed error.

--- aecid-testsuite/runTryItOut.sh
+++ aecid-testsuite/runTryItOut.sh
@@ -4,7 +4,7 @@ sudo chown -R aminer:aminer /var/lib/aminer #2> /dev/null
 # the second ```python script is searched for.
 git clone https://github.com/ait-aecid/logdata-anomaly-miner.wiki.git #2> /dev/null
 cd logdata-anomaly-miner.wiki #2> /dev/null
-git checkout development > #/dev/null 2>&1
+git checkout development #> /dev/null 2>&1
 cd ..
 awk '/^```python$/ && ++n == 2, /^```$/' < logdata-anomaly-miner.wiki/AMiner-TryItOut.md | sed '/^```/ d' | sed '/^```python/ d' > /tmp/tryItOut-config.yml
 sudo rm -r logdata-anomaly-miner.wiki
