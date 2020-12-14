2671a7f584f3b24506dca58f255f256c6d21c7cc
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2020-12-14 13:55:10 +0100

    removed messages from runTryItOut test.

--- aecid-testsuite/runTryItOut.sh
+++ aecid-testsuite/runTryItOut.sh
@@ -1,10 +1,10 @@
-sudo chown -R aminer:aminer /var/lib/aminer #2> /dev/null
+sudo chown -R aminer:aminer /var/lib/aminer 2> /dev/null
 
 # extract the file from the development branch of the wiki project.
 # the second ```python script is searched for.
-git clone https://github.com/ait-aecid/logdata-anomaly-miner.wiki.git #2> /dev/null
-cd logdata-anomaly-miner.wiki #2> /dev/null
-git checkout development #> /dev/null 2>&1
+git clone https://github.com/ait-aecid/logdata-anomaly-miner.wiki.git 2> /dev/null
+cd logdata-anomaly-miner.wiki 2> /dev/null
+git checkout development > /dev/null 2>&1
 cd ..
 awk '/^```python$/ && ++n == 2, /^```$/' < logdata-anomaly-miner.wiki/AMiner-TryItOut.md | sed '/^```/ d' | sed '/^```python/ d' > /tmp/tryItOut-config.yml
 sudo rm -r logdata-anomaly-miner.wiki
