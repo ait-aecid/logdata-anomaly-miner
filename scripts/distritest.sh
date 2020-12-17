a70302ed9c674c17a709b65b22b00e595cb63730
Wolfgang Hotwagner <wolfgang.hotwagner@ait.ac.at>
2020-12-17 19:29:48 +0100

    distritest: return-value of test is not adequate

--- scripts/distritest.sh
+++ scripts/distritest.sh
@@ -5,4 +5,4 @@ sudo service apache2 start
 curl localhost
 curl -XPOST localhost
 curl -I localhost
-sudo timeout --preserve-status 20s aminer --config /home/aminer/gettingStarted-config.yml
+sudo timeout 20s aminer --config /home/aminer/gettingStarted-config.yml
