238c18600b7b968826f3d23943badb0f3bd5bf8c
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2021-02-01 16:55:32 +0100

    fixed issues.

--- aecid-testsuite/demo/aminer/aminerDemo.sh
+++ aecid-testsuite/demo/aminer/aminerDemo.sh
@@ -22,12 +22,14 @@ if [[ $sudoInstalled == 0 ]]; then
 	sudo mkdir /tmp/lib/aminer 2> /dev/null
 	sudo chown -R $USER:$USER /tmp/lib/aminer 2> /dev/null
 	sudo rm -r /tmp/lib/aminer/* 2> /dev/null
+	sudo mkdir /tmp/lib/aminer/log 2> /dev/null
 	sudo chown -R aminer:aminer /tmp/lib/aminer 2> /dev/null
 	sudo rm /tmp/syslog 2> /dev/null
 else
 	mkdir /tmp/lib 2> /dev/null
 	mkdir /tmp/lib/aminer 2> /dev/null
 	rm -r /tmp/lib/aminer/* 2> /dev/null
+	mkdir /tmp/lib/aminer/log 2> /dev/null
 	chown -R aminer:aminer /tmp/lib/aminer 2> /dev/null
 	rm /tmp/syslog 2> /dev/null
 fi
