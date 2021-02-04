5bdb4dd918967f220f71ccf8c5854a7bcce7798e
Wolfgang Hotwagner <wolfgang.hotwagner@ait.ac.at>
2021-02-04 13:42:57 +0100

    make wiki-tests branch-dependent

--- aecid-testsuite/runTryItOut.sh
+++ aecid-testsuite/runTryItOut.sh
@@ -1,10 +1,20 @@
+#!/bin/bash
+
+BRANCH=main
+
+if [ $# -gt 0 ]
+then
+BRANCH=$1
+fi
+
+
 sudo chown -R aminer:aminer /var/lib/aminer 2> /dev/null
 
 # extract the file from the development branch of the wiki project.
 # the second ```python script is searched for.
 git clone https://github.com/ait-aecid/logdata-anomaly-miner.wiki.git 2> /dev/null
 cd logdata-anomaly-miner.wiki 2> /dev/null
-git checkout development > /dev/null 2>&1
+git checkout $BRANCH > /dev/null 2>&1
 cd ..
 awk '/^```python$/ && ++n == 2, /^```$/' < logdata-anomaly-miner.wiki/AMiner-TryItOut.md | sed '/^```/ d' | sed '/^```python/ d' > /tmp/tryItOut-config.yml
 sudo rm -r logdata-anomaly-miner.wiki
