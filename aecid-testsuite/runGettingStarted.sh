bdd5dde3fb08e6de7a433954b9f33ed2ad1524e4
Wolfgang Hotwagner <wolfgang.hotwagner@ait.ac.at>
2021-02-04 13:42:57 +0100

    make wiki-tests branch-dependent

--- aecid-testsuite/runGettingStarted.sh
+++ aecid-testsuite/runGettingStarted.sh
@@ -1,10 +1,19 @@
+#!/bin/bash
+
+BRANCH=main
+
+if [ $# -gt 0 ]
+then
+BRANCH=$1
+fi
+
 sudo chown -R aminer:aminer /var/lib/aminer 2> /dev/null
 
 # extract the file from the development branch of the wiki project.
 # the first ```yaml script is searched for.
 git clone https://github.com/ait-aecid/logdata-anomaly-miner.wiki.git 2> /dev/null
 cd logdata-anomaly-miner.wiki 2> /dev/null
-git checkout development > /dev/null 2>&1
+git checkout $BRANCH > /dev/null 2>&1
 cd ..
 awk '/^```yaml$/ && ++n == 1, /^```$/' < logdata-anomaly-miner.wiki/Getting-started-\(tutorial\).md | sed '/^```/ d' | sed '/^```python/ d' > /tmp/gettingStarted-config.yml
 sudo rm -r logdata-anomaly-miner.wiki
