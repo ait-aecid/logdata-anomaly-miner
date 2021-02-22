78390de9e87fbc4abe4d41f6f91b1cfefb7f7705
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2021-02-22 14:05:54 +0100

    fixed runAminerElasticsearchDemo.sh.

--- aecid-testsuite/runAminerElasticsearchDemo.sh
+++ aecid-testsuite/runAminerElasticsearchDemo.sh
@@ -1,7 +1,7 @@
 cp demo/aminerElasticsearchDemo/elasticsearch-demo-config.yml /tmp/elasticsearch-demo-config.yml
 sudo chown -R aminer:aminer /tmp/lib 2> /dev/null
 sudo chmod +x demo/aminerElasticsearchDemo/aminerElasticsearchDemo.sh
-sudo ./demo/aminer/aminerElasticsearchDemo.sh > /dev/null
+sudo ./demo/aminerElasticsearchDemo/aminerElasticsearchDemo.sh > /dev/null
 exit_code=$?
 sudo rm /tmp/elasticsearch-demo-config.yml 2> /dev/null
 sudo rm /tmp/syslog
