4309dcc11d02efb4de0fb37690717b200734a3df
dITySoftware <56120276+dITySoftware@users.noreply.github.com>
2021-03-08 08:37:05 +0100

    034 empty validation schemas (#506)
    * added DetectionTimestamp to json output.
    
    * fixed unittests.
    
    * fixed unittests.
    
    * fixed KafkaEventHandlerTest.
    
    * fixed testingwrapper.
    
    * fixed runAminerElasticsearchDemo.sh
    
    * undid changes in testingwrapper.
    
    * resolved conflicts
    
    * fixed kafka tests.
    
    * extended schemas to check if strings are empty.
    
    * fixed names in demo configs.
    
    Co-authored-by: Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>

--- source/root/usr/lib/logdata-anomaly-miner/aminer/schemas/validation/ParserValidationSchema.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/schemas/validation/ParserValidationSchema.py
@@ -8,25 +8,25 @@
             'allow_unknown': False,
             'oneof_schema': [
                 {
-                    'id': {'type': 'string', 'required': True},
+                    'id': {'type': 'string', 'required': True, 'empty': False},
                     'start': {'type': 'boolean'},
-                    'type': {'type': 'string', 'forbidden': [
+                    'type': {'type': 'string', 'empty': False, 'forbidden': [
                         'ElementValueBranchModelElement', 'DecimalIntegerValueModelElement', 'DecimalIntegerValueModelElement',
                         'MultiLocaleDateTimeModelElement', 'DelimitedDataModelElement', 'JsonModelElement'], 'required': True},
-                    'name': {'type': 'string', 'required': True},
+                    'name': {'type': 'string', 'required': True, 'empty': False},
                     'args': {'type': ['string', 'list'], 'schema': {'type': ['string', 'integer']}}
                 },
                 {
-                    'id': {'type': 'string', 'required': True},
+                    'id': {'type': 'string', 'required': True, 'empty': False},
                     'start': {'type': 'boolean'},
                     'type': {'type': 'string', 'allowed': ['ElementValueBranchModelElement'], 'required': True},
-                    'name': {'type': 'string', 'required': True},
+                    'name': {'type': 'string', 'required': True, 'empty': False},
                     'args': {'type': ['string', 'list'], 'schema': {'type': ['string', 'integer']}, 'required': True},
-                    'branch_model_dict': {'type': 'list', 'schema': {'type': 'dict', 'schema': {'id': {
-                         'type': ['boolean', 'float', 'integer', 'string']}, 'model': {'type': 'string'}}}, 'required': True}
+                    'branch_model_dict': {'type': 'list', 'schema': {'type': 'dict', 'schema': {'id': {'type': [
+                        'boolean', 'float', 'integer', 'string']}, 'model': {'type': 'string', 'empty': False}}}, 'required': True}
                 },
                 {
-                    'id': {'type': 'string', 'required': True},
+                    'id': {'type': 'string', 'required': True, 'empty': False},
                     'start': {'type': 'boolean'},
                     'type': {'type': 'string', 'allowed': ['DecimalFloatValueModelElement'], 'required': True},
                     'name': {'type': 'string', 'required': True},
@@ -35,36 +35,36 @@
                     'exponent_type': {'type': 'string', 'allowed': ['none', 'optional', 'mandatory']}
                 },
                 {
-                    'id': {'type': 'string', 'required': True},
+                    'id': {'type': 'string', 'required': True, 'empty': False},
                     'start': {'type': 'boolean'},
                     'type': {'type': 'string', 'allowed': ['DecimalIntegerValueModelElement'], 'required': True},
-                    'name': {'type': 'string', 'required': True},
+                    'name': {'type': 'string', 'required': True, 'empty': False},
                     'value_sign_type': {'type': 'string', 'allowed': ['none', 'optional', 'mandatory']},
                     'value_pad_type': {'type': 'string', 'allowed': ['none', 'zero', 'blank']}
                 },
                 {
-                    'id': {'type': 'string', 'required': True},
+                    'id': {'type': 'string', 'required': True, 'empty': False},
                     'start': {'type': 'boolean'},
                     'type': {'type': 'string', 'allowed': ['MultiLocaleDateTimeModelElement'], 'required': True},
-                    'name': {'type': 'string', 'required': True},
+                    'name': {'type': 'string', 'required': True, 'empty': False},
                     'date_formats': {'type': 'list', 'schema': {'type': 'dict', 'schema': {'format': {'type': 'list', 'schema': {
-                        'type': 'string', 'nullable': True}, 'maxlength': 3, 'minlength': 3}}}, 'required': True},
+                        'type': 'string', 'nullable': True, 'empty': False}, 'maxlength': 3, 'minlength': 3}}}, 'required': True},
                     'start_year': {'type': 'integer', 'nullable': True}
                 },
                 {
-                    'id': {'type': 'string', 'required': True},
+                    'id': {'type': 'string', 'required': True, 'empty': False},
                     'start': {'type': 'boolean'},
                     'type': {'type': 'string', 'allowed': ['DelimitedDataModelElement'], 'required': True},
-                    'name': {'type': 'string', 'required': True},
-                    'delimiter': {'type': 'string', 'required': True},
+                    'name': {'type': 'string', 'required': True, 'empty': False},
+                    'delimiter': {'type': 'string', 'required': True, 'empty': False},
                     'escape': {'type': 'string'},
                     'consume_delimiter': {'type': 'boolean'}
                 },
                 {
-                    'id': {'type': 'string', 'required': True},
+                    'id': {'type': 'string', 'required': True, 'empty': False},
                     'start': {'type': 'boolean'},
                     'type': {'type': 'string', 'allowed': ['JsonModelElement'], 'required': True},
-                    'name': {'type': 'string', 'required': True},
+                    'name': {'type': 'string', 'required': True, 'empty': False},
                     'key_parser_dict': {'type': 'dict', 'required': True},
                     'optional_key_prefix': {'type': 'string'}
                 },
