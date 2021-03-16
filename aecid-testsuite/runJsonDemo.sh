69bbf304317a1696386fe2d8d758e20593f6628f
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2021-03-16 14:39:19 +0100

    removed sudoInstalled from test scripts.

--- aecid-testsuite/runJsonDemo.sh
+++ aecid-testsuite/runJsonDemo.sh
@@ -1,24 +1,10 @@
 AMINER_PERSISTENCE_PATH=/tmp/lib/aminer/*
-sudoInstalled=`dpkg -s sudo | grep Status 2> /dev/null`
 
-if [[ $sudoInstalled == "Status: install ok installed" ]]; then
-	sudoInstalled=0
-else
-	sudoInstalled=1
-fi
-	
-if [[ $sudoInstalled == 0 ]]; then
-	sudo mkdir /tmp/lib 2> /dev/null
-	sudo mkdir /tmp/lib/aminer 2> /dev/null
-	sudo chown -R $USER:$USER /tmp/lib/aminer 2> /dev/null
-	sudo rm -r $AMINER_PERSISTENCE_PATH 2> /dev/null
-	sudo chown -R aminer:aminer /tmp/lib/aminer 2> /dev/null
-else
-	mkdir /tmp/lib 2> /dev/null
-	mkdir /tmp/lib/aminer 2> /dev/null
-	rm -r $AMINER_PERSISTENCE_PATH 2> /dev/null
-	chown -R aminer:aminer /tmp/lib/aminer 2> /dev/null
-fi
+sudo mkdir /tmp/lib 2> /dev/null
+sudo mkdir /tmp/lib/aminer 2> /dev/null
+sudo chown -R $USER:$USER /tmp/lib/aminer 2> /dev/null
+sudo rm -r $AMINER_PERSISTENCE_PATH 2> /dev/null
+sudo chown -R aminer:aminer /tmp/lib/aminer 2> /dev/null
 
 cp -r ./demo/aminerJsonInputDemo/json_logs /tmp/json_logs
 
