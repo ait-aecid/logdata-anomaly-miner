8978c75e2a577409ec92e33b2565e103a521d0f3
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2021-03-24 13:32:55 +0100

    added comparison methods to TestBase. updated unittests for FixedDataModelElement.

--- aecid-testsuite/unit/parsing/FixedDataModelElementTest.py
+++ aecid-testsuite/unit/parsing/FixedDataModelElementTest.py
@@ -10,35 +10,33 @@ class FixedDataModelElementTest(TestBase):
 
     def test1get_id(self):
         """Test if get_id works properly."""
-        fixed_dme = FixedDataModelElement("s0", b'fixed data.')
+        fixed_dme = FixedDataModelElement("s0", b"fixed data.")
         self.assertEqual(fixed_dme.get_id(), "s0")
 
     def test2get_child_elements(self):
         """Test if get_child_elements returns None."""
-        fixed_dme = FixedDataModelElement("s0", b'fixed data.')
+        fixed_dme = FixedDataModelElement("s0", b"fixed data.")
         self.assertEqual(fixed_dme.get_child_elements(), None)
 
     def test3get_match_element_valid_match(self):
         """Parse matching substring from MatchContext and check if the MatchContext was updated accordingly."""
-        fixed_string = b'fixed data.'
+        fixed_string = b"fixed data."
+        id_ = "path"
+        path = "fixed"
         match_context = DummyMatchContext(self.data)
-        fixed_dme = FixedDataModelElement("s0", fixed_string)
-        match_element = fixed_dme.get_match_element("fixed", match_context)
-        self.assertEqual(match_element.path, "fixed/s0")
-        self.assertEqual(match_element.match_string, fixed_string)
-        self.assertEqual(match_element.match_object, fixed_string)
-        self.assertIsNone(match_element.children, None)
-        self.assertEqual(match_context.match_string, fixed_string)
+        fixed_dme = FixedDataModelElement(id_, fixed_string)
+        match_element = fixed_dme.get_match_element(path, match_context)
+        self.compare_match_results(self.data, match_element, match_context, id_, path, fixed_string, fixed_string, None)
 
     def test4get_match_element_no_match(self):
         """Parse not matching substring from MatchContext and check if the MatchContext was not changed."""
         no_match_string = b"Hello World."
+        id_ = "path"
+        path = "fixed"
         match_context = DummyMatchContext(self.data)
-        fixed_dme = FixedDataModelElement("s0", no_match_string)
-        match_element = fixed_dme.get_match_element("fixed", match_context)
-        self.assertIsNone(match_element, None)
-        self.assertEqual(match_context.match_data, self.data)
-        self.assertEqual(match_context.match_string, b"")
+        fixed_dme = FixedDataModelElement(id_, no_match_string)
+        match_element = fixed_dme.get_match_element(path, match_context)
+        self.compare_no_match_results(self.data, match_element, match_context)
 
     def test5element_id_input_validation(self):
         """Check if element_id is validated."""
@@ -46,6 +44,10 @@ class FixedDataModelElementTest(TestBase):
         # empty element_id
         element_id = ""
         self.assertRaises(ValueError, FixedDataModelElement, element_id, fixed_string)
+
+        # None element_id
+        element_id = None
+        self.assertRaises(TypeError, FixedDataModelElement, element_id, fixed_string)
 
         # bytes element_id is not allowed
         element_id = b"path"
@@ -67,12 +69,20 @@ class FixedDataModelElementTest(TestBase):
         element_id = ["path"]
         self.assertRaises(TypeError, FixedDataModelElement, element_id, fixed_string)
 
+        # empty list element_id is not allowed
+        element_id = []
+        self.assertRaises(TypeError, FixedDataModelElement, element_id, fixed_string)
+
     def test6fixed_data_input_validation(self):
         """Check if fixed_data is validated."""
         element_id = "path"
         # empty fixed_string
         fixed_string = b""
         self.assertRaises(ValueError, FixedDataModelElement, element_id, fixed_string)
+
+        # None fixed_string
+        fixed_string = None
+        self.assertRaises(TypeError, FixedDataModelElement, element_id, fixed_string)
 
         # string fixed_string is not allowed
         fixed_string = "path"
@@ -92,6 +102,10 @@ class FixedDataModelElementTest(TestBase):
 
         # list fixed_string is not allowed
         fixed_string = ["path"]
+        self.assertRaises(TypeError, FixedDataModelElement, element_id, fixed_string)
+
+        # empty list fixed_string is not allowed
+        fixed_string = []
         self.assertRaises(TypeError, FixedDataModelElement, element_id, fixed_string)
 
 
