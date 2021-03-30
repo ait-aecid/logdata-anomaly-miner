a7737a33e10ba4eb16febf1c98de0afb68f167f0
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2021-03-30 23:15:11 +0200

    fixed deepsource issues.

--- aecid-testsuite/unit/analysis/VariableTypeDetectorTest.py
+++ aecid-testsuite/unit/analysis/VariableTypeDetectorTest.py
@@ -601,7 +601,7 @@ class VariableTypeDetectorTest(TestBase):
                         variable_type_ini = variable_type_ini[:-1]
                     break
                 if 'uni' in [distr[0] for distr in variable_type_ini[-1]]:
-                    for j, val in enumerate(variable_type_ini[-1]):
+                    for _, val in enumerate(variable_type_ini[-1]):
                         if val[0] == 'uni':
                             variable_type_ini = val
                             break
@@ -625,7 +625,7 @@ class VariableTypeDetectorTest(TestBase):
                     break
                 if 'nor' in [distr[0] for distr in variable_type_ini[-1]]:
                     for j, val in enumerate(variable_type_ini[-1]):
-                        if vval[0] == 'nor':
+                        if val[0] == 'nor':
                             variable_type_ini = val
                             break
 
