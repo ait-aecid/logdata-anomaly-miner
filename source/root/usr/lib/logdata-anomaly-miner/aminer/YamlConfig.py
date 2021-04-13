5ffef5af6a25ea5a6c149d0dd4c930c428e70b8c
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2021-04-13 12:19:48 +0200

    fixed errors.

--- source/root/usr/lib/logdata-anomaly-miner/aminer/YamlConfig.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/YamlConfig.py
@@ -45,26 +45,26 @@ def load_yaml(config_file):
             logging.getLogger(DEBUG_LOG_NAME).error(exception)
             raise exception
 
-    with open(os.path.dirname(os.path.abspath(__file__)) + '/' + 'schemas/BaseSchema.json', 'r') as sma:
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/' + 'schemas/BaseSchema.yml', 'r') as sma:
         # skipcq: PYL-W0123
         base_schema = eval(sma.read())
-    with open(os.path.dirname(os.path.abspath(__file__)) + '/' + 'schemas/normalisation/ParserNormalisationSchema.json', 'r') as sma:
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/' + 'schemas/normalisation/ParserNormalisationSchema.yml', 'r') as sma:
         # skipcq: PYL-W0123
         parser_normalisation_schema = eval(sma.read())
-    with open(os.path.dirname(os.path.abspath(__file__)) + '/' + 'schemas/normalisation/AnalysisNormalisationSchema.json', 'r') as sma:
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/' + 'schemas/normalisation/AnalysisNormalisationSchema.yml', 'r') as sma:
         # skipcq: PYL-W0123
         analysis_normalisation_schema = eval(sma.read())
-    with open(os.path.dirname(os.path.abspath(__file__)) + '/' + 'schemas/normalisation/EventHandlerNormalisationSchema.json', 'r') as sma:
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/' + 'schemas/normalisation/EventHandlerNormalisationSchema.yml', 'r') as sma:
         # skipcq: PYL-W0123
         event_handler_normalisation_schema = eval(sma.read())
 
-    with open(os.path.dirname(os.path.abspath(__file__)) + '/' + 'schemas/validation/ParserValidationSchema.json', 'r') as sma:
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/' + 'schemas/validation/ParserValidationSchema.yml', 'r') as sma:
         # skipcq: PYL-W0123
         parser_validation_schema = eval(sma.read())
-    with open(os.path.dirname(os.path.abspath(__file__)) + '/' + 'schemas/validation/AnalysisValidationSchema.json', 'r') as sma:
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/' + 'schemas/validation/AnalysisValidationSchema.yml', 'r') as sma:
         # skipcq: PYL-W0123
         analysis_validation_schema = eval(sma.read())
-    with open(os.path.dirname(os.path.abspath(__file__)) + '/' + 'schemas/validation/EventHandlerValidationSchema.json', 'r') as sma:
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/' + 'schemas/validation/EventHandlerValidationSchema.yml', 'r') as sma:
         # skipcq: PYL-W0123
         event_handler_validation_schema = eval(sma.read())
 
