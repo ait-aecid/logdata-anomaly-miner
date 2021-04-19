904145e008eacdceefe26aff247415eda57908a6
dITySoftware <56120276+dITySoftware@users.noreply.github.com>
2021-04-19 10:23:20 +0200

    073 override parameters (#622)
    * added code to allow overriding config_properties from the command line.
    
    * fixed deepsource issues.
    
    * fixed errors.
    
    * undid changes.
    
    * added handling of lists.
    
    * added exception handling and a warning if the variable does not exist in the config.
    
    Co-authored-by: Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>

--- source/root/usr/lib/logdata-anomaly-miner/aminer.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer.py
@@ -36,6 +36,8 @@ import shutil
 import warnings
 import argparse
 import stat
+import tempfile
+import ast
 from pwd import getpwnam
 from grp import getgrnam
 
@@ -55,7 +57,7 @@ child_termination_triggered_flag = False
 
 def run_analysis_child(aminer_config, program_name):
     """Run the Analysis Child."""
-    # Verify existance and ownership of persistence directory.
+    # Verify existence and ownership of persistence directory.
     logging.getLogger(AminerConfig.REMOTE_CONTROL_LOG_NAME).info('aminer started.')
     logging.getLogger(AminerConfig.DEBUG_LOG_NAME).info('aminer started.')
     persistence_dir_name = aminer_config.config_properties.get(AminerConfig.KEY_PERSISTENCE_DIR, AminerConfig.DEFAULT_PERSISTENCE_DIR)
@@ -180,6 +182,35 @@ def initialize_loggers(aminer_config, aminer_user_id, aminer_grp_id):
     debug_logger.addHandler(debug_file_handler)
 
 
+def parse_var(s):
+    """
+    Parse a key, value pair, separated by "=".
+    That's the reverse of ShellArgs.
+
+    On the command line (argparse) a declaration will typically look like:
+        foo=hello
+    or
+        foo="hello world"
+    """
+    items = s.split("=")
+    key = items[0].strip()  # we remove blanks around keys, as is logical
+    if len(items) > 1:
+        # rejoin the rest:
+        value = "=".join(items[1:])
+    return key, value
+
+
+def parse_vars(items):
+    """Parse a series of key-value pairs and return a dictionary."""
+    d = {}
+
+    if items:
+        for item in items:
+            key, value = parse_var(item)
+            d[key] = value
+    return d
+
+
 def main():
     """Run the aminer main program."""
     # Extract program name, but only when sure to contain no problematic characters.
@@ -214,6 +245,11 @@ def main():
     parser.add_argument('-r', '--remove', action='append', type=str, help='removes a specific persistence directory')
     parser.add_argument('-R', '--restore', type=str, help='restore a persistence backup')
     parser.add_argument('-f', '--from-begin', action='store_true', help='removes RepositioningData before starting the aminer')
+    parser.add_argument("--config-properties", metavar="KEY=VALUE", nargs='+',
+                        help="Set a number of config_properties by using key-value pairs (do not put spaces before or after the = sign). "
+                             "If a value contains spaces, you should define it with double quotes: 'foo=\"this is a sentence\". Note that "
+                             "values are always treated as strings. If values are already defined in the config_properties, the input "
+                             "types are converted to the ones already existing.")
 
     args = parser.parse_args()
 
@@ -266,13 +302,73 @@ def main():
         print('%s: config "%s" not (yet) available!' % (program_name, config_file_name), file=sys.stderr)
         sys.exit(1)
 
+    # using the solution here to override config_properties:
+    # https://stackoverflow.com/questions/27146262/create-variable-key-value-pairs-with-argparse-python
+    use_temp_config = False
+    config_properties = parse_vars(args.config_properties)
+    if args.config_properties and "LearnMode" in config_properties:
+        ymlext = [".YAML", ".YML", ".yaml", ".yml"]
+        extension = os.path.splitext(config_file_name)[1]
+        if extension in ymlext:
+            use_temp_config = True
+            fd, temp_config = tempfile.mkstemp(suffix=".yml")
+            with open(config_file_name) as f:
+                for line in f:
+                    if "LearnMode" in line:
+                        line = "LearnMode: %s" % config_properties["LearnMode"]
+                    os.write(fd, line.encode())
+            config_file_name = temp_config
+            os.close(fd)
+        else:
+            msg = "The LearnMode parameter does not exist in .py configs!"
+            print(msg, sys.stderr)
+            logging.getLogger(AminerConfig.DEBUG_LOG_NAME).error(msg)
+            sys.exit(1)
+
     # Minimal import to avoid loading too much within the privileged process.
     try:
         aminer_config = AminerConfig.load_config(config_file_name)
+        if use_temp_config:
+            os.remove(config_file_name)
+            config_file_name = args.config
     except ValueError as e:
         sys.exit(1)
-    persistence_dir = aminer_config.config_properties.get(AminerConfig.KEY_PERSISTENCE_DIR, AminerConfig.DEFAULT_PERSISTENCE_DIR)
 
+    for config_property in config_properties:
+        if config_property == "LearnMode":
+            continue
+        old_value = aminer_config.config_properties.get(config_property)
+        value = config_properties[config_property]
+        if old_value is not None:
+            try:
+                if isinstance(old_value, bool):
+                    if value == "True":
+                        value = True
+                    elif value == "False":
+                        value = False
+                    else:
+                        msg = "The %s parameter must be of type %s!" % (config_property, type(old_value))
+                        print(msg, sys.stderr)
+                        logging.getLogger(AminerConfig.DEBUG_LOG_NAME).error(msg)
+                        sys.exit(1)
+                elif isinstance(old_value, int):
+                    value = int(value)
+                elif isinstance(old_value, float):
+                    value = float(value)
+                elif isinstance(old_value, list):
+                    value = ast.literal_eval(value)
+            except ValueError:
+                msg = "The %s parameter must be of type %s!" % (config_property, type(old_value))
+                print(msg, sys.stderr)
+                logging.getLogger(AminerConfig.DEBUG_LOG_NAME).error(msg)
+                sys.exit(1)
+        else:
+            msg = "The %s parameter is not set in the config. It will be treated as a string!" % config_property
+            print("WARNING: " + msg, sys.stderr)
+            logging.getLogger(AminerConfig.DEBUG_LOG_NAME).warning(msg)
+        aminer_config.config_properties[config_property] = value
+
+    persistence_dir = aminer_config.config_properties.get(AminerConfig.KEY_PERSISTENCE_DIR, AminerConfig.DEFAULT_PERSISTENCE_DIR)
     child_user_name = aminer_config.config_properties.get(AminerConfig.KEY_AMINER_USER)
     child_group_name = aminer_config.config_properties.get(AminerConfig.KEY_AMINER_GROUP)
     child_user_id = -1
@@ -562,9 +658,13 @@ def main():
         # Now execute the very same program again, but user might have moved or renamed it meanwhile. This would be problematic with
         # SUID-binaries (which we do not yet support). Do NOT just fork but also exec to avoid child circumventing
         # parent's ALSR due to cloned kernel VMA.
-        execArgs = ['aminerChild', '--run-analysis', '--config', analysis_config_file_name, '--stat', str(stat_level), '--debug',
-                    str(debug_level)]
-        os.execve(sys.argv[0], execArgs, {})  # skipcq: BAN-B606
+        exec_args = ['aminerChild', '--run-analysis', '--config', analysis_config_file_name, '--stat', str(stat_level), '--debug',
+                     str(debug_level)]
+        if args.config_properties:
+            exec_args.append("--config-properties")
+            for config_property in args.config_properties:
+                exec_args.append(config_property)
+        os.execve(sys.argv[0], exec_args, {})  # skipcq: BAN-B606
         msg = 'Failed to execute child process'
         print(msg, file=sys.stderr)
         logging.getLogger(AminerConfig.DEBUG_LOG_NAME).error(msg)
