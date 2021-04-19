80a4dcfb75060f51de7faa4132521ee2189699aa
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2021-04-19 17:53:12 +0200

    removed deprecated unittest.

--- aecid-testsuite/unit/events/JsonConverterHandlerTest.py
+++ aecid-testsuite/unit/events/JsonConverterHandlerTest.py
@@ -47,26 +47,6 @@ class JsonConverterHandlerTest(TestBase):
                 self.description, self.__class__.__name__, self.description, self.event_message, self.persistence_id, round(self.t, 2),
                 detection_timestamp, ""))
 
-    def test2receive_event_with_same_event_data_attributes(self):
-        """In this test case an attribute of AnalysisComponent is overwritten and an JsonError attribute is expected."""
-        json_converter_handler = JsonConverterHandler([self.stream_printer_event_handler], self.analysis_context)
-        log_atom = LogAtom(self.fixed_dme.fixed_data, ParserMatch(self.match_element), self.t, self)
-        self.analysis_context.register_component(self, self.description)
-        event_data = {'AnalysisComponent': {'AffectedParserPaths': ['test/path/1', 'test/path/2'],
-                      'Message': 'An other event happened too!'}}
-        json_converter_handler.receive_event(self.test_detector, self.event_message, self.sorted_log_lines,
-                                             event_data, log_atom, self)
-        detection_timestamp = None
-        for line in self.output_stream.getvalue().split('\n'):
-            if "DetectionTimestamp" in line:
-                detection_timestamp = line.split(':')[1].strip(' ,')
-        self.assertEqual(
-            self.output_stream.getvalue(), self.expected_string % (
-                datetime.fromtimestamp(self.t).strftime("%Y-%m-%d %H:%M:%S"), self.event_message, self.__class__.__name__,
-                self.description, self.__class__.__name__, self.description, self.event_message, self.persistence_id, round(self.t, 2),
-                detection_timestamp,
-                ',\n  "JsonError": "AnalysisComponent attribute \'Message\' is already in use and can not be overwritten!\\n"'))
-
 
 if __name__ == '__main__':
     unittest.main()
