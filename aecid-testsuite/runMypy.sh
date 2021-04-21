8dd6b8da1b02c2fe4644d619fac166ead7a24840
dITySoftware <56120276+dITySoftware@users.noreply.github.com>
2021-04-21 13:16:37 +0200

    fixed mypy tests. (#641)
    * fixed mypy tests.
    
    * fixed paths.
    
    * moved imports to /etc/aminer
    
    Co-authored-by: Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>

--- aecid-testsuite/runMypy.sh
+++ aecid-testsuite/runMypy.sh
@@ -1,6 +1,14 @@
-mypy source/root/usr/lib/logdata-anomaly-miner/aminer/analysis/ --ignore-missing-imports
-mypy source/root/usr/lib/logdata-anomaly-miner/aminer/events/ --ignore-missing-imports
-mypy source/root/usr/lib/logdata-anomaly-miner/aminer/input/ --ignore-missing-imports
-mypy source/root/usr/lib/logdata-anomaly-miner/aminer/parsing/ --ignore-missing-imports
-mypy source/root/usr/lib/logdata-anomaly-miner/aminer/util/ --ignore-missing-imports
-mypy source/root/usr/lib/logdata-anomaly-miner/aminer/ --ignore-missing-imports
+exit_code=0
+mypy /usr/lib/logdata-anomaly-miner/aminer/analysis/ --ignore-missing-imports
+exit_code=$(($exit_code + $?))
+mypy /usr/lib/logdata-anomaly-miner/aminer/events/ --ignore-missing-imports
+exit_code=$(($exit_code + $?))
+mypy /usr/lib/logdata-anomaly-miner/aminer/input/ --ignore-missing-imports
+exit_code=$(($exit_code + $?))
+mypy /usr/lib/logdata-anomaly-miner/aminer/parsing/ --ignore-missing-imports
+exit_code=$(($exit_code + $?))
+mypy /usr/lib/logdata-anomaly-miner/aminer/util/ --ignore-missing-imports
+exit_code=$(($exit_code + $?))
+mypy /usr/lib/logdata-anomaly-miner/aminer/ --ignore-missing-imports
+exit_code=$(($exit_code + $?))
+exit $exit_code
