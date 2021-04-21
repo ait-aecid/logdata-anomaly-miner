a05e1069feb685fce48af50864ff03df105d6303
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2021-04-21 08:52:58 +0200

    fixed paths.

--- aecid-testsuite/runMypy.sh
+++ aecid-testsuite/runMypy.sh
@@ -1,14 +1,14 @@
 exit_code=0
-mypy ../source/root/usr/lib/logdata-anomaly-miner/aminer/analysis/ --ignore-missing-imports
+mypy /usr/lib/logdata-anomaly-miner/aminer/analysis/ --ignore-missing-imports
 exit_code=$(($exit_code + $?))
-mypy ../source/root/usr/lib/logdata-anomaly-miner/aminer/events/ --ignore-missing-imports
+mypy /usr/lib/logdata-anomaly-miner/aminer/events/ --ignore-missing-imports
 exit_code=$(($exit_code + $?))
-mypy ../source/root/usr/lib/logdata-anomaly-miner/aminer/input/ --ignore-missing-imports
+mypy /usr/lib/logdata-anomaly-miner/aminer/input/ --ignore-missing-imports
 exit_code=$(($exit_code + $?))
-mypy ../source/root/usr/lib/logdata-anomaly-miner/aminer/parsing/ --ignore-missing-imports
+mypy /usr/lib/logdata-anomaly-miner/aminer/parsing/ --ignore-missing-imports
 exit_code=$(($exit_code + $?))
-mypy ../source/root/usr/lib/logdata-anomaly-miner/aminer/util/ --ignore-missing-imports
+mypy /usr/lib/logdata-anomaly-miner/aminer/util/ --ignore-missing-imports
 exit_code=$(($exit_code + $?))
-mypy ../source/root/usr/lib/logdata-anomaly-miner/aminer/ --ignore-missing-imports
+mypy /usr/lib/logdata-anomaly-miner/aminer/ --ignore-missing-imports
 exit_code=$(($exit_code + $?))
 exit $exit_code
