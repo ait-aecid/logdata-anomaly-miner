418a44d89391c002a4890f587dafc4c038f9fdc5
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2021-04-25 14:50:21 +0200

    rewrote unittests for the OptionalMatchModelElement.

--- source/root/usr/lib/logdata-anomaly-miner/aminer/parsing/OptionalMatchModelElement.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/parsing/OptionalMatchModelElement.py
@@ -11,7 +11,8 @@ FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with
 this program. If not, see <http://www.gnu.org/licenses/>.
 """
-
+import logging
+from aminer.AminerConfig import DEBUG_LOG_NAME
 from aminer.parsing.MatchElement import MatchElement
 from aminer.parsing.ModelElementInterface import ModelElementInterface
 
@@ -22,9 +23,24 @@ class OptionalMatchModelElement(ModelElementInterface):
     If that fails returns a zero length match anyway.
     """
 
-    def __init__(self, element_id, optional_element):
+    def __init__(self, element_id: str, optional_element: ModelElementInterface):
+        if not isinstance(element_id, str):
+            msg = "element_id has to be of the type string."
+            logging.getLogger(DEBUG_LOG_NAME).error(msg)
+            raise TypeError(msg)
+        if len(element_id) < 1:
+            msg = "element_id must not be empty."
+            logging.getLogger(DEBUG_LOG_NAME).error(msg)
+            raise ValueError(msg)
         self.element_id = element_id
+
+        if not isinstance(optional_element, ModelElementInterface):
+            msg = "optional_element has to be of the type ModelElementInterface."
+            logging.getLogger(DEBUG_LOG_NAME).error(msg)
+            raise TypeError(msg)
         self.optional_element = optional_element
+        self.empty_match_element = MatchElement("%s/%s" % ("None", self.element_id), b"None", None, None)
+        self.empty_match_element.match_string = b""
 
     def get_id(self):
         """Get the element ID."""
@@ -34,14 +50,15 @@ class OptionalMatchModelElement(ModelElementInterface):
         """Return all optional elements."""
         return [self.optional_element]
 
-    def get_match_element(self, path, match_context):
+    def get_match_element(self, path: str, match_context):
         """@return the embedded child match or an empty match."""
         current_path = "%s/%s" % (path, self.element_id)
 
         start_data = match_context.match_data
         match = self.optional_element.get_match_element(current_path, match_context)
         if match is None:
-            return MatchElement("%s/%s" % (path, self.element_id), '', None, None)
+            self.empty_match_element.path = current_path
+            return self.empty_match_element
 
         return MatchElement(current_path, start_data[:len(start_data) - len(match_context.match_data)],
                             start_data[:len(start_data) - len(match_context.match_data)], [match])
