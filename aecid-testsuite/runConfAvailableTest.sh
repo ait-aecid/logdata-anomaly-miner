405f492bf160e5efef4e7f8f06c024351bd67d72
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2021-07-15 14:41:10 +0200

    added code to find all configs.

--- aecid-testsuite/runConfAvailableTest.sh
+++ aecid-testsuite/runConfAvailableTest.sh
@@ -1,1 +1,29 @@
-cat ../source/root/etc/aminer/conf-available/ait-lds/ApacheAccessParsingModel.py
+#!/bin/bash
+
+PATH_AIT_LDS=../source/root/etc/aminer/conf-available/ait-lds/*.py
+#PATH_AIT_LDS=/etc/aminer/conf-available/ait-lds/*.py
+
+cat > /tmp/config.py <<EOL
+LearnMode: False # optional
+Core.PersistenceDir: '/tmp/lib/aminer'
+
+LogResourceList:
+        - 'file:///tmp/log.txt'
+
+Input:
+        timestamp_paths: ["/accesslog/time"]
+        verbose: True # use this to debug your parser-model
+
+EventHandlers:
+        - id: stpe
+          type: StreamPrinterEventHandler
+EOL
+
+for filename in $PATH_AIT_LDS; do
+    basename "$filename"
+    BN=`basename "$filename"`
+    case $BN in
+        ApacheAccessParsingModel.py)
+            echo True
+    esac
+done
