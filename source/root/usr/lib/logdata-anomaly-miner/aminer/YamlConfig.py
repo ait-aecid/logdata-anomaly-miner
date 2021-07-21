7922585a2427599f92db1edf77ae2c42de4cf3b3
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2021-07-21 13:37:40 +0200

    changed yamlconfig.

--- source/root/usr/lib/logdata-anomaly-miner/aminer/YamlConfig.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/YamlConfig.py
@@ -129,7 +129,7 @@ def build_analysis_pipeline(analysis_context):
     """
     parsing_model = build_parsing_model()
     anomaly_event_handlers, atom_filter = build_input_pipeline(analysis_context, parsing_model)
-    build_analysis_components(analysis_context, anomaly_event_handlers, atom_filter)
+    build_analysis_components(analysis_context, anomaly_event_handlers, atom_filter, parsing_model)
     event_handler_id_list = build_event_handlers(analysis_context, anomaly_event_handlers)
     # do not check UnparsedAtomHandler
     for index, analysis_component in enumerate(atom_filter.subhandler_list[1:]):
@@ -312,15 +312,6 @@ def build_input_pipeline(analysis_context, parsing_model):
     analysis_context.atomizer_factory = SimpleByteStreamLineAtomizerFactory(
         parsing_model, atom_handler_list, anomaly_event_handlers, default_timestamp_paths=timestamp_paths, eol_sep=eol_sep,
         json_format=json_format)
-    # Just report all unparsed atoms to the event handlers.
-    if yaml_data['Input']['verbose'] is True:
-        from aminer.input.VerboseUnparsedAtomHandler import VerboseUnparsedAtomHandler
-        atom_filter.add_handler(VerboseUnparsedAtomHandler(anomaly_event_handlers, parsing_model), stop_when_handled_flag=True)
-    else:
-        from aminer.input.SimpleUnparsedAtomHandler import SimpleUnparsedAtomHandler
-        atom_filter.add_handler(SimpleUnparsedAtomHandler(anomaly_event_handlers), stop_when_handled_flag=True)
-    if yaml_data['Input']['suppress_unparsed'] is True:
-        analysis_context.suppress_detector_list.append(atom_filter.subhandler_list[-1][0])
     from aminer.analysis.NewMatchPathDetector import NewMatchPathDetector
     if 'LearnMode' in yaml_data:
         learn = yaml_data['LearnMode']
@@ -333,11 +324,12 @@ def build_input_pipeline(analysis_context, parsing_model):
     return anomaly_event_handlers, atom_filter
 
 
-def build_analysis_components(analysis_context, anomaly_event_handlers, atom_filter):
+def build_analysis_components(analysis_context, anomaly_event_handlers, atom_filter, parsing_model):
     """Build the analysis components."""
     suppress_detector_list = analysis_context.suppress_detector_list
     if yaml_data['SuppressNewMatchPathDetector']:
         suppress_detector_list.append('DefaultNewMatchPathDetector')
+    has_unparsed_handler = False
     if 'Analysis' in yaml_data and yaml_data['Analysis'] is not None:
         analysis_dict = {}
         match_action_dict = {}
@@ -354,6 +346,7 @@ def build_analysis_components(analysis_context, anomaly_event_handlers, atom_fil
                 break
 
         for item in yaml_data['Analysis']:
+            stop_when_handled_flag = False
             if item['id'] == 'None':
                 comp_name = None
             else:
@@ -776,12 +769,23 @@ def build_analysis_components(analysis_context, anomaly_event_handlers, atom_fil
                     num_max_time_history=item['num_max_time_history'], num_results_bt=item['num_results_bt'], alpha_bt=item['alpha_bt'],
                     acf_threshold=item['acf_threshold'], round_time_inteval_threshold=item['round_time_inteval_threshold'],
                     output_log_line=item['output_logline'], ignore_list=item['ignore_list'])
+            elif item["type"].name == "VerboseUnparsedAtomHandler":
+                has_unparsed_handler = True
+                stop_when_handled_flag = True
+                tmp_analyser = func(anomaly_event_handlers, parsing_model)
+            elif item["type"].name == "SimpleUnparsedAtomHandler":
+                has_unparsed_handler = True
+                stop_when_handled_flag = True
+                tmp_analyser = func(anomaly_event_handlers)
             else:
                 tmp_analyser = func(analysis_context.aminer_config, item['paths'], anomaly_event_handlers, auto_include_flag=learn)
             if item['output_event_handlers'] is not None:
                 tmp_analyser.output_event_handlers = item['output_event_handlers']
             analysis_context.register_component(tmp_analyser, component_name=comp_name)
-            atom_filter.add_handler(tmp_analyser)
+            atom_filter.add_handler(tmp_analyser, stop_when_handled_flag=stop_when_handled_flag)
+    if not has_unparsed_handler:
+        from aminer.analysis.UnparsedAtomHandlers import VerboseUnparsedAtomHandler
+        atom_filter.add_handler(VerboseUnparsedAtomHandler(anomaly_event_handlers, parsing_model))
 
 
 def build_event_handlers(analysis_context, anomaly_event_handlers):
