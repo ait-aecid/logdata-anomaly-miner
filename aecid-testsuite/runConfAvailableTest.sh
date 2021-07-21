459bc54c5c5ba9002576df219e34eb0aefba5633
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2021-07-21 22:41:36 +0200

    fixed error.

--- aecid-testsuite/runConfAvailableTest.sh
+++ aecid-testsuite/runConfAvailableTest.sh
@@ -35,7 +35,6 @@ for filename in ${files[@]}; do
     cat > $CONFIG_PATH <<EOL
 LearnMode: False
 Core.PersistenceDir: '/tmp/lib/aminer'
-Core.LogDir: '/tmp/lib/aminer/log'
 
 LogResourceList:
         - 'file://$LOG_FILE'
