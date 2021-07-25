3c8edb5ae13cf74e37f70407af59b2b192e90607
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2021-07-25 16:32:45 +0200

    .

--- source/root/usr/lib/logdata-anomaly-miner/aminer/parsing/JsonModelElement.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/parsing/JsonModelElement.py
@@ -29,9 +29,7 @@ debug_log_prefix = "JsonModelElement: "
 
 
 def format_float(val):
-    """
-    This function formats the float-value and parses the sign and the exponent.
-    """
+    """This function formats the float-value and parses the sign and the exponent."""
     exp = None
     if "e" in val:
         exp = "e"
