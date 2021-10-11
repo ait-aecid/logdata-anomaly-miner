6bdc009621398d391bc0e25d3b43c9265d82508a
whotwagner <ilovemail@feedyourhead.at>
2021-10-11 08:48:30 +0200

    Added redpanda to docker-compose (#971)
    This commit adds a redpanda(kafka) container to the docker-compose file.
    It also improves the prepare-script so that docker-compose works out of
    the box.
    
    Co-authored-by: Wolfgang Hotwagner <wolfgang.hotwagner@ait.ac.at>

--- scripts/prep-docker-compose.sh
+++ scripts/prep-docker-compose.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 test -d aminercfg || mkdir aminercfg
+test -d persistency || mkdir persistency
+test -d persistency/log || mkdir persistency/log
+test -d logs || mkdir logs
+test -d akafka || mkdir akafka
 test -e aminercfg/config.yml || cp -r source/root/etc/aminer/template_config.yml aminercfg/config.yml
 test -d aminercfg/conf-enabled || mkdir aminercfg/conf-enabled
 test -e aminercfg/conf-enabled/ApacheAccessModel.py || cp source/root/etc/aminer/conf-available/generic/ApacheAccessModel.py aminercfg/conf-enabled/ApacheAccessModel.py
