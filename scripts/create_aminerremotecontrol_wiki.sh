983f7420df41d026e3b44781c0b07731e723f88a
Ernst Leierzopf <ernst.leierzopf.fl@ait.ac.at>
2022-02-01 00:56:45 +0100

    extended script to fix wiki page.

--- scripts/create_aminerremotecontrol_wiki.sh
+++ scripts/create_aminerremotecontrol_wiki.sh
@@ -1,6 +1,7 @@
+cd /usr/share/man/man1/
 sudo xsltproc --output /usr/share/man/man1/aminerremotecontrol.1  -''-nonet -''-param man.charmap.use.subset "0" -''-param make.year.ranges "1" -''-param make.single.year.ranges "1" /usr/share/xml/docbook/stylesheet/docbook-xsl/manpages/docbook.xsl /home/user/Documents/Git_projects/logdata-anomaly-miner/debian/aminerremotecontrol.1.xml && sudo gzip /usr/share/man/man1/aminerremotecontrol.1
 sudo gunzip /usr/share/man/man1/aminerremotecontrol.1.gz
-sudo mv /usr/share/man/man1/aminerremotecontrol.1 /tmp
+sudo cp /usr/share/man/man1/aminerremotecontrol.1 /tmp
 sudo chown user:user /tmp/aminerremotecontrol.1
 sudo apt install pandoc
 pandoc --from man --to gfm /tmp/aminerremotecontrol.1 -o /tmp/aminerremotecontrol.md # man-to-github-flawored-markdown
@@ -15,6 +16,7 @@ sed -i $'s/,old\\\_component\\\_name,/,\'old\\\_component\\\_name\',/g' /tmp/ami
 sed -i $'s/,new\\\_component\\\_name/,\'new\\\_component\\\_name\'/g' /tmp/aminerremotecontrol.md
 sed -i $'s/,history\\\_component\\\_name/,\'history\\\_component\\\_name\'/g' /tmp/aminerremotecontrol.md
 sed -i $'s/NewMatchPathDetector/\'NewMatchPathDetector\'/g' /tmp/aminerremotecontrol.md
+sed -i $'s/\*\'NewMatchPathDetector\'\*/\*NewMatchPathDetector\*/g' /tmp/aminerremotecontrol.md
 sed -i $'s/,component\\\_name/,\'component\\\_name\'/g' /tmp/aminerremotecontrol.md
 sed -i $'s/AtomFilter/,\'AtomFilter\'/g' /tmp/aminerremotecontrol.md
 sed -i $'s/LogResourceList/\'LogResourceList\'/g' /tmp/aminerremotecontrol.md
@@ -44,3 +46,14 @@ sed -i $'s/,Core.PersistencePeriod,/,\'Core.PersistencePeriod\',/g' /tmp/aminerr
 sed -i $'s/,Log.StatisticsLevel,/,\'Log.StatisticsLevel\',/g' /tmp/aminerremotecontrol.md
 sed -i $'s/,Log.DebugLevel,/,\'Log.DebugLevel\',/g' /tmp/aminerremotecontrol.md
 sed -i $'s/,Log.StatisticsPeriod,/,\'Log.StatisticsPeriod\',/g' /tmp/aminerremotecontrol.md
+sed -i $'s/\*\*\*socket/\*\* \*socket/g' /tmp/aminerremotecontrol.md
+sed -i $'s/\*\*\*command/\*\* \*command/g' /tmp/aminerremotecontrol.md
+sed -i $'s/\*\*\*file/\*\* \*file/g' /tmp/aminerremotecontrol.md
+sed -i $'s/\*\*\*data/\*\* \*data/g' /tmp/aminerremotecontrol.md
+sed -i $'s/command\*\*\*/command\* \*\*/g' /tmp/aminerremotecontrol.md
+sed -i $'s/file\*\*\*/file\* \*\*/g' /tmp/aminerremotecontrol.md
+sed -i $'s/^\*\*$//g' /tmp/aminerremotecontrol.md
+sed -i $'s/^\*\* \*\*\*/\*\*\*/g' /tmp/aminerremotecontrol.md
+sed -i $'s/\*\*\* \*\*/\*\*\*/g' /tmp/aminerremotecontrol.md
+sed -i $'s/\*\*\*/\*\*/g' /tmp/aminerremotecontrol.md
+sed -i ':a;N;$!ba;s/\*\*aminerremotecontrol\*\* \\\[\*\*\\\[--exec \*\* \*command\* \*\*\\] | \\\[--exec-file\n\*\* \*file\* \*\*\\]\*\*\\] \*\*\\\[OPTIONS\\]...\*\*/\*\*aminerremotecontrol\*\* \\\[\*\*\\\[--exec \*\* \*command\* \*\*\\] | \\\[--exec-file\*\* \*file\* \*\*\\]\*\*\\] \*\*\\\[OPTIONS\\]...\*\*/g' /tmp/aminerremotecontrol.md
