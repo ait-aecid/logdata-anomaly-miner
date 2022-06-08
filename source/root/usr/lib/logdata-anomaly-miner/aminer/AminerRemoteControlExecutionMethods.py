c7fbf5d477f6ed2c798d506e132ce1391047cb17
hoeldg <66625424+hoeldg@users.noreply.github.com>
2022-06-08 15:04:17 +0200

    TID: Added remote controll events print and remove from persistency. … (#1126)
    * TID: Added remote controll events print and remove from persistency. Furthermore the id_tupple format was changed from bytes to string
    
    * Fixed DeepSourceIssue

--- source/root/usr/lib/logdata-anomaly-miner/aminer/AminerRemoteControlExecutionMethods.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/AminerRemoteControlExecutionMethods.py
@@ -422,10 +422,10 @@ class AminerRemoteControlExecutionMethods:
         if component is None:
             self.REMOTE_CONTROL_RESPONSE += "FAILURE: component '%s' does not exist!" % component
             return
-        if component.__class__.__name__ not in ["MinimalTransitionTimeDetector"]:
+        if component.__class__.__name__ not in ["MinimalTransitionTimeDetector", "PathValueTimeIntervalDetector"]:
             self.REMOTE_CONTROL_RESPONSE += \
                 "FAILURE: component class '%s' does not support the print_persistency_event! Only the following classes support it: " \
-                "MinimalTransitionTimeDetector." \
+                "MinimalTransitionTimeDetector and PathValueTimeIntervalDetector." \
                 % component.__class__.__name__
             return
         try:
@@ -473,10 +473,10 @@ class AminerRemoteControlExecutionMethods:
         if component is None:
             self.REMOTE_CONTROL_RESPONSE += "FAILURE: component '%s' does not exist!" % component
             return
-        if component.__class__.__name__ not in ["MinimalTransitionTimeDetector"]:
+        if component.__class__.__name__ not in ["MinimalTransitionTimeDetector", "PathValueTimeIntervalDetector"]:
             self.REMOTE_CONTROL_RESPONSE += \
                 "FAILURE: component class '%s' does not support the remove_from_persistency_event! Only the following classes support it: "\
-                "MinimalTransitionTimeDetector." \
+                "MinimalTransitionTimeDetector and PathValueTimeIntervalDetector." \
                 % component.__class__.__name__
             return
         try:
