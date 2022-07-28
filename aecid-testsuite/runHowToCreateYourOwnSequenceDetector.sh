1e5ec054cea274499c6838b15588c1c8020a6a21
hoeldg <georg.hoeld@ait.ac.at>
2022-07-28 12:46:55 +0200

    Fixed a bug, that runHowToCreateYourOwnSequenceDetector.sh would not correctly isert code in YamlConfig.

--- aecid-testsuite/runHowToCreateYourOwnSequenceDetector.sh
+++ aecid-testsuite/runHowToCreateYourOwnSequenceDetector.sh
@@ -49,7 +49,7 @@ sudo cp $YML_CONFIG $TMP_YML_CONFIG
 # add code to YamlConfig.py
 printf "            " > $TMP_SCHEMA
 awk '/^```python$/ && ++n == 2, /^```$/' < $SRC_FILE | sed '/^```/ d' >> $TMP_SCHEMA
-sudo sed -i "                    /num_sections_waiting_time_for_tsa=item/r $TMP_SCHEMA" $YML_CONFIG
+sudo sed -i "                    /max_rule_attributes=item/r $TMP_SCHEMA" $YML_CONFIG
 
 runAminerUntilEnd "sudo aminer --config $CFG_PATH -C" "" "/var/lib/aminer/AnalysisChild/RepositioningData" "$CFG_PATH" "/dev/null"
 exit_code=$?
