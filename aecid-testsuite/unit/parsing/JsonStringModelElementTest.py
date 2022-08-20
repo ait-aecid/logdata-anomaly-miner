1b151fa1b184a07386f65689db9c923e4209501e
Wolfgang Hotwagner <wolfgang.hotwagner@ait.ac.at>
2022-08-20 21:35:30 +0200

    Add tests for ignore_null(JsonStringModelElement)
    This commit adds testcases for the ignore_null parameter of the
    JsonStringModelElement.

--- aecid-testsuite/unit/parsing/JsonStringModelElementTest.py
+++ aecid-testsuite/unit/parsing/JsonStringModelElementTest.py
@@ -12,10 +12,12 @@ class JsonStringModelElementTest(TestBase):
     """Unittests for the JsonStringModelElement."""
     id_ = "json"
     path = "path"
+    strict = False
+    ignore_null = True
 
     def test1get_id(self):
         """Test if get_id works properly."""
-        json_me = JsonStringModelElement(self.id_, {})
+        json_me = JsonStringModelElement(self.id_, {}, self.strict, self.ignore_null)
         self.assertEqual(json_me.get_id(), self.id_)
 
     def test2get_match_element_valid_match(self):
@@ -25,7 +27,7 @@ class JsonStringModelElementTest(TestBase):
 
         key_parser_dict = { "host": host, "user": user }
 
-        json_model_element = JsonStringModelElement(self.id_, key_parser_dict)
+        json_model_element = JsonStringModelElement(self.id_, key_parser_dict, self.strict, self.ignore_null)
         data = b'{"host": "www.google.com", "user": "foobar", "one": "two"}'
         match_context = DummyMatchContext(data)
         match_element = json_model_element.get_match_element(self.path, match_context)
@@ -42,7 +44,7 @@ class JsonStringModelElementTest(TestBase):
         key_parser_dict = { "host": { "server": host }, "user": user }
 
         # Sets strict_mode to True
-        json_model_element = JsonStringModelElement(self.id_, key_parser_dict, True)
+        json_model_element = JsonStringModelElement(self.id_, key_parser_dict, True, self.ignore_null)
         # "one": "two" is too much
         data = b'{"host": {"server": "www.google.com"}, "user": "foobar", "one": "two"}'
         match_context = DummyMatchContext(data)
@@ -60,7 +62,7 @@ class JsonStringModelElementTest(TestBase):
         match_element = json_model_element.get_match_element(self.path, match_context)
         self.assertEqual(None,match_element)
 
-        # Sets the logdata to the exact configuration-json. 
+        # Sets the logdata to the exact configuration-json.
         data = b'{"host": {"server": "www.google.com"}, "user": "foobar", "path": "/index.html"}'
         match_context = DummyMatchContext(data)
         match_element = json_model_element.get_match_element(self.path, match_context)
@@ -68,3 +70,78 @@ class JsonStringModelElementTest(TestBase):
         self.assertEqual(b"www.google.com", match_element.children[0].get_match_object())
         self.assertEqual(b"foobar", match_element.children[1].get_match_object())
         self.assertEqual(b"/index.html", match_element.children[2].get_match_object())
+
+    def test4ignore_null(self):
+        """Parses a json-file with ignore_null and compares if the configured ModelElements are parsed properly"""
+        host = DummyFixedDataModelElement("host", b"www.google.com")
+        user = DummyFixedDataModelElement("user", b"foobar")
+
+        key_parser_dict = { "host": host, "user": user }
+
+        # Set ignore_null to True and strict to False
+        json_model_element = JsonStringModelElement(self.id_, key_parser_dict, False, True)
+        # Set user to null
+        data = b'{"host": "www.google.com", "user": null, "one": "two"}'
+        match_context = DummyMatchContext(data)
+        match_element = json_model_element.get_match_element(self.path, match_context)
+        # Line must be parsed but without "user":
+        self.assertEqual(1, len(match_element.children))
+        self.assertEqual(b"www.google.com", match_element.children[0].get_match_object())
+        
+        # set ignore_null to False and strict to False
+        json_model_element = JsonStringModelElement(self.id_, key_parser_dict, False, False)
+        # Set user to null
+        data = b'{"host": "www.google.com", "user": null, "one": "two"}'
+        match_context = DummyMatchContext(data)
+        match_element = json_model_element.get_match_element(self.path, match_context)
+        # expect an unparsed line
+        self.assertEqual(None,match_element)
+
+        # set example user to empty string
+        user = DummyFixedDataModelElement("user", b"")
+        key_parser_dict2 = { "host": host, "user": user }
+
+        # Set ignore_null to False in order to pass b"" to the subparser. Strict is False
+        json_model_element = JsonStringModelElement(self.id_, key_parser_dict2, False, False)
+        # Set user to null
+        data = b'{"host": "www.google.com", "user": null}'
+        match_context = DummyMatchContext(data)
+        match_element = json_model_element.get_match_element(self.path, match_context)
+        # Line must be parsed:
+        self.assertEqual(2, len(match_element.children))
+        self.assertEqual(b"www.google.com", match_element.children[0].get_match_object())
+        self.assertEqual(b"", match_element.children[1].get_match_object())
+
+        # Set ignore_null to True and strict to True
+        json_model_element = JsonStringModelElement(self.id_, key_parser_dict, True, True)
+        # Set user to null
+        data = b'{"host": "www.google.com", "user": null}'
+        match_context = DummyMatchContext(data)
+        match_element = json_model_element.get_match_element(self.path, match_context)
+        # Line must be parsed but without "user":
+        self.assertEqual(1, len(match_element.children))
+        self.assertEqual(b"www.google.com", match_element.children[0].get_match_object())
+        
+        # set ignore_null to False and strict to True
+        json_model_element = JsonStringModelElement(self.id_, key_parser_dict, True, False)
+        # Set user to null
+        data = b'{"host": "www.google.com", "user": null}'
+        match_context = DummyMatchContext(data)
+        match_element = json_model_element.get_match_element(self.path, match_context)
+        # expect an unparsed line
+        self.assertEqual(None,match_element)
+
+        # set example user to empty string
+        user = DummyFixedDataModelElement("user", b"")
+        key_parser_dict2 = { "host": host, "user": user }
+
+        # Set ignore_null to False in order to pass b"" to the subparser. Strict is True
+        json_model_element = JsonStringModelElement(self.id_, key_parser_dict2, True, False)
+        # Set user to null
+        data = b'{"host": "www.google.com", "user": null}'
+        match_context = DummyMatchContext(data)
+        match_element = json_model_element.get_match_element(self.path, match_context)
+        # Line must be parsed:
+        self.assertEqual(2, len(match_element.children))
+        self.assertEqual(b"www.google.com", match_element.children[0].get_match_object())
+        self.assertEqual(b"", match_element.children[1].get_match_object())
