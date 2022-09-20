49d158a26fd7af013486dd50da7e6e1b13c2c29e
Ernst Leierzopf <e.leierzopf@gmail.com>
2022-09-20 14:43:34 +0200

    added input file path sanitization and fixed exception handling.

--- source/root/usr/lib/logdata-anomaly-miner/aminer/AminerRemoteControlExecutionMethods.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/AminerRemoteControlExecutionMethods.py
@@ -212,7 +212,7 @@ class AminerRemoteControlExecutionMethods:
             elif '.' in val:
                 try:
                     val = float(val)
-                except:  # skipcq: FLK-E722
+                except ValueError:  # skipcq: FLK-E722
                     pass
         self.REMOTE_CONTROL_RESPONSE = '"%s": %s' % (property_name, val)
 
@@ -326,8 +326,11 @@ class AminerRemoteControlExecutionMethods:
         @param analysis_context the analysis context of the aminer.
         @param destination_file the path to the file in which the config is saved.
         """
-        msg = AminerConfig.save_config(analysis_context, destination_file)
-        self.REMOTE_CONTROL_RESPONSE = msg
+        if re.match("^(/[^/ ]*)+/?$", destination_file) is not None:
+            msg = AminerConfig.save_config(analysis_context, destination_file)
+            self.REMOTE_CONTROL_RESPONSE = msg
+        else:
+            msg = f"Exception: {destination_file} is not a valid filename!"
         logging.getLogger(DEBUG_LOG_NAME).info(msg)
 
     def persist_all(self):
@@ -735,6 +738,6 @@ def _reformat_attr(attr):
     if not isinstance(attr, (list, dict, tuple, set)) and not rep.startswith('"') and not rep.isdecimal():
         try:
             float(rep)
-        except:  # skipcq: FLK-E722
+        except ValueError:  # skipcq: FLK-E722
             rep = '"%s"' % rep
     return rep
