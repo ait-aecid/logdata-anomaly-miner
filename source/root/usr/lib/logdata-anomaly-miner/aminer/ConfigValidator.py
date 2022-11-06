ad28d890abfafb373c901a686aedc6c687fc88bd
Ernst Leierzopf <e.leierzopf@gmail.com>
2022-11-06 18:04:37 +0100

    fixed bug.

--- source/root/usr/lib/logdata-anomaly-miner/aminer/ConfigValidator.py
+++ source/root/usr/lib/logdata-anomaly-miner/aminer/ConfigValidator.py
@@ -51,11 +51,11 @@ class ParserModelType:
                             logging.getLogger(DEBUG_LOG_NAME).error(exception)
                             raise exception
 
-                    with open(os.path.dirname(os.path.abspath(__file__)) + '/' + 'schemas/normalisation/ParserNormalisationSchema.yml',
+                    with open(os.path.dirname(os.path.abspath(__file__)) + '/' + 'schemas/normalisation/ParserNormalisationSchema.py',
                               'r') as sma:
                         # skipcq: PYL-W0123
                         parser_normalisation_schema = eval(sma.read())
-                    with open(os.path.dirname(os.path.abspath(__file__)) + '/' + 'schemas/validation/ParserValidationSchema.yml',
+                    with open(os.path.dirname(os.path.abspath(__file__)) + '/' + 'schemas/validation/ParserValidationSchema.py',
                               'r') as sma:
                         # skipcq: PYL-W0123
                         parser_validation_schema = eval(sma.read())
