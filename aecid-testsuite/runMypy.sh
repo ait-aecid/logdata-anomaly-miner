8dbf18bb857fe582819c5122971093de229b912e
Ernst Leierzopf <e.leierzopf@gmail.com>
2023-10-30 07:20:00 +0100

    fix mypy.

--- aecid-testsuite/runMypy.sh
+++ aecid-testsuite/runMypy.sh
@@ -1,14 +1,14 @@
 exit_code=0
-mypy /usr/lib/logdata-anomaly-miner/aminer/analysis/ --ignore-missing-imports
+mypy /usr/lib/logdata-anomaly-miner/aminer/analysis/ --ignore-missing-imports --disable-error-code attr-defined --implicit-optional
 exit_code=$(($exit_code + $?))
-mypy /usr/lib/logdata-anomaly-miner/aminer/events/ --ignore-missing-imports
+mypy /usr/lib/logdata-anomaly-miner/aminer/events/ --ignore-missing-imports --disable-error-code attr-defined --implicit-optional
 exit_code=$(($exit_code + $?))
-mypy /usr/lib/logdata-anomaly-miner/aminer/input/ --ignore-missing-imports
+mypy /usr/lib/logdata-anomaly-miner/aminer/input/ --ignore-missing-imports --disable-error-code attr-defined --implicit-optional
 exit_code=$(($exit_code + $?))
-mypy /usr/lib/logdata-anomaly-miner/aminer/parsing/ --ignore-missing-imports
+mypy /usr/lib/logdata-anomaly-miner/aminer/parsing/ --ignore-missing-imports --disable-error-code attr-defined --implicit-optional
 exit_code=$(($exit_code + $?))
-mypy /usr/lib/logdata-anomaly-miner/aminer/util/ --ignore-missing-imports
+mypy /usr/lib/logdata-anomaly-miner/aminer/util/ --ignore-missing-imports --disable-error-code attr-defined --implicit-optional
 exit_code=$(($exit_code + $?))
-mypy /usr/lib/logdata-anomaly-miner/aminer/ --ignore-missing-imports
+mypy /usr/lib/logdata-anomaly-miner/aminer/ --ignore-missing-imports --disable-error-code attr-defined --implicit-optional
 exit_code=$(($exit_code + $?))
 exit $exit_code
