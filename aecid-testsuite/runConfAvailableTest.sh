3fb9430349f58d4e5061a42b913c5ec92b73a9ee
Ernst Leierzopf <e.leierzopf@gmail.com>
2024-04-29 16:29:41 +0200

    test

--- aecid-testsuite/runConfAvailableTest.sh
+++ aecid-testsuite/runConfAvailableTest.sh
@@ -718,7 +718,7 @@ EOL
 
     runAminerUntilEnd "sudo aminer -C -c $CONFIG_PATH" "$LOGFILE" "/tmp/lib/aminer/AnalysisChild/RepositioningData" "$CONFIG_PATH" > $OUT 2>&1
     exit_code=$?
-    cat $OUTPUT
+    cat $OUT
 
     if [[ `grep -ic "VerboseUnparsedAtomHandler" $OUT` != 0 && $BN != "AminerParsingModel" ]] || [[ `grep -o '\bVerboseUnparsedAtomHandler\b' $OUT | wc -l` > 5 ]] || `grep -Fq "Traceback" $OUT` || `grep -Fq "{'Parser'" $OUT` || `grep -Fq "FATAL" $OUT` || `grep -Fq "Config-Error" $OUT`; then
       echo "Failed Test in $filename"
