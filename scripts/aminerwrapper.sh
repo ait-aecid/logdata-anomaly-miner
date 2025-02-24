6f781e508b7ff7045d11b07a5336abdcf0c90abd
Ernst Leierzopf <e.leierzopf@gmail.com>
2025-02-24 14:27:39 +0100

    fix intendations in aminerwrapper.

--- scripts/aminerwrapper.sh
+++ scripts/aminerwrapper.sh
@@ -4,17 +4,17 @@ AMINERDIR=/usr/lib/logdata-anomaly-miner
 
 case "$1" in
 	aminer)
-	    $AMINERDIR/.venv/bin/activate
+	  $AMINERDIR/.venv/bin/activate
 		$AMINERDIR/aminer.py ${*:2}
 		deactivate
 		;;
 	aminerremotecontrol)
-	    $AMINERDIR/.venv/bin/activate
+	  $AMINERDIR/.venv/bin/activate
 		$AMINERDIR/aminerremotecontrol.py ${*:2}
 		deactivate
 		;;
     aminer-persistence)
-	    $AMINERDIR/.venv/bin/activate
+	  $AMINERDIR/.venv/bin/activate
 		$AMINERDIR/aminer-persistence.py ${*:2}
 		deactivate
 		;;
