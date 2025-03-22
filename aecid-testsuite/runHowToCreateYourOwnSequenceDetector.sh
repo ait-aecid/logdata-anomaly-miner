04f92b49f74f0f3d5f9e649a1a088e8d8e9fbc3e
Ernst Leierzopf <e.leierzopf@gmail.com>
2025-03-22 13:55:03 +0100

    undo changes.

--- aecid-testsuite/runHowToCreateYourOwnSequenceDetector.sh
+++ aecid-testsuite/runHowToCreateYourOwnSequenceDetector.sh
@@ -80,7 +80,6 @@ awk '/^```$/ && ++n == 2, /^```$/ && n++ == 3' < $INPUT | sed '/^```/ d' > $LOG
 #PID=$!
 #sleep 5
 #sudo pkill -x aminer.py
-#sudo pkill -x aminer
 #wait $PID
 #OUT2=`cat $OUT`
 #compareStrings "$OUT1" "$OUT2" "Failed Test in 2."
