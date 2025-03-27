9e93efef10fa707acf2e991005f45fb5e88a775b
Ernst Leierzopf <e.leierzopf@gmail.com>
2025-03-28 00:31:44 +0100

    test.

--- aecid-testsuite/runHowToCreateYourOwnSequenceDetector.sh
+++ aecid-testsuite/runHowToCreateYourOwnSequenceDetector.sh
@@ -82,6 +82,7 @@ awk '/^```$/ && ++n == 2, /^```$/ && n++ == 3' < $INPUT | sed '/^```/ d' > $LOG
 #sudo pkill -x aminer.py
 #sudo pkill -x aminer
 #wait $PID
+#
 #OUT2=`cat $OUT`
 #compareStrings "$OUT1" "$OUT2" "Failed Test in 2."
 #exit_code=$?
